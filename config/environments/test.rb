2185dbbaab3f0c9ffe0d71bd6f5dcc60171d8335
Matt Jankowski <mjankowski@thoughtbot.com>
2008-10-02 11:06:41 -0400

    require things from test_helper, configure thing in the env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,6 +20,3 @@ config.action_controller.allow_forgery_protection    = false
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
-
-require 'mocha'
-require 'factory_girl'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,14 +2,16 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 require 'quietbacktrace'
+require 'factory_girl'
 require 'redgreen'
+require 'mocha'
 
 class Test::Unit::TestCase
-  
+
   self.use_transactional_fixtures = true
   self.use_instantiated_fixtures  = false
-  
+
   self.backtrace_silencers << :rails_vendor
   self.backtrace_filters   << :rails_root
-  
+
 end
