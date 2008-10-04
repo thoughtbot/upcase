6a9d07099e0d80a904929dc004a68342d088c736
Matt Jankowski <mjankowski@thoughtbot.com>
2008-10-03 20:44:19 -0400

    dont use redgreen

--- config/environment.rb
+++ config/environment.rb
@@ -35,7 +35,6 @@ Rails::Initializer.run do |config|
   config.gem 'thoughtbot-factory_girl', 
              :lib => 'factory_girl', 
              :source => 'http://gems.github.com'
-  config.gem "redgreen"
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
