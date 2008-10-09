65f405c544d5afc6648022475805eb4d18dd32e5
Matt Jankowski <mjankowski@thoughtbot.com>
2008-10-09 11:35:28 -0400

    update version and name information for shoulda and will_paginate in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -28,13 +28,15 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', 
              :lib => 'will_paginate', 
              :source => 'http://gems.github.com', 
-             :version => '~> 2.3.4'
+             :version => '~> 2.3.5'
   config.gem 'thoughtbot-shoulda', 
-             :lib => 'shoulda/rails', 
+             :lib => 'shoulda',
              :source => 'http://gems.github.com'
+             :version => '>= 2.0.4'
   config.gem 'thoughtbot-factory_girl', 
              :lib => 'factory_girl', 
              :source => 'http://gems.github.com'
+             :version => '>= 1.1.3'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
