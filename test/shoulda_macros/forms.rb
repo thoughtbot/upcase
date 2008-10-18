a648442e0e424d186db1546133a93f565f1c0ee7
Dan Croak <dcroak@thoughtbot.com>
2008-10-17 21:19:56 -0400

    making should_have_form better

--- test/shoulda_macros/forms.rb
+++ test/shoulda_macros/forms.rb
@@ -1,10 +1,13 @@
 class Test::Unit::TestCase
- def self.should_have_form(opts)
-    model = opts[:model] || self.name.gsub(/ControllerTest$/, '').singularize.downcase
+  def self.should_have_form(opts)
+    model = self.name.gsub(/ControllerTest$/, '').singularize.downcase
     model = model[model.rindex('::')+2..model.size] if model.include?('::')
-    http_method = opts[:method].nil? ? 'post' : opts[:method].to_s
+    http_method, hidden_http_method = form_http_method opts[:method]
     should "have a #{model} form" do
       assert_select "form[action=?][method=#{http_method}]", eval(opts[:action]) do
+        if hidden_http_method
+          assert_select "input[type=hidden][name=_method][value=#{hidden_http_method}]"
+        end
         opts[:fields].each do |attribute, type|
           attribute = attribute.is_a?(Symbol) ? "#{model}[#{attribute.to_s}]" : attribute
           assert_select "input[type=#{type.to_s}][name=?]", attribute
