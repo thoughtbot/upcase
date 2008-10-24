8a91466f2f809e368ad4ba30c1c6c45ef99ad776
Matt Jankowski <mjankowski@thoughtbot.com>
2008-10-24 13:54:22 -0400

    use av:tc instead of helper_testcase

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
-require File.expand_path(File.dirname(__FILE__) + '/helper_testcase')
+require 'action_view/test_case'
 
 class Test::Unit::TestCase
 
@@ -11,4 +11,11 @@ class Test::Unit::TestCase
   self.backtrace_silencers << :rails_vendor
   self.backtrace_filters   << :rails_root
 
+end
+
+class ActionView::TestCase
+  # Enable UrlWriter when testing helpers
+  include ActionController::UrlWriter
+  # Default host for helper tests
+  default_url_options[:host] = HOST
 end
