193938dd28a95e52f150888b5d33ac6947b8468c
Matt Jankowski <mjankowski@thoughtbot.com>
2008-12-08 14:45:22 -0500

    rake rails:freeze:edge RELEASE=2.2.2

--- vendor/rails/actionpack/lib/action_controller/vendor/html-scanner.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH << "#{File.dirname(__FILE__)}/html-scanner"
-
-module HTML
-  autoload :Document, 'html/document'
-  autoload :Sanitizer, 'html/sanitizer'
-  autoload :FullSanitizer, 'html/sanitizer'
-  autoload :LinkSanitizer, 'html/sanitizer'
-  autoload :WhiteListSanitizer, 'html/sanitizer'
-end
