82f85f107d5b2c7b9609f14678216ce34a0501c4
Matt Jankowski <mjankowski@thoughtbot.com>
2008-12-13 15:43:37 -0500

    update shoulda, factory girl and mocha gem versions

--- config/environment.rb
+++ config/environment.rb
@@ -31,19 +31,19 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', 
              :lib => 'will_paginate', 
              :source => 'http://gems.github.com', 
-             :version => '~> 2.3.5'
+             :version => '~> 2.3.6'
   config.gem 'mocha', 
-             :version => '>= 0.9.2'
+             :version => '>= 0.9.3'
   config.gem 'quietbacktrace', 
              :version => '>= 0.1.1'
   config.gem 'thoughtbot-factory_girl', 
              :lib => 'factory_girl', 
              :source => 'http://gems.github.com', 
-             :version => '>= 1.1.3'
+             :version => '>= 1.1.5'
   config.gem 'thoughtbot-shoulda', 
              :lib => 'shoulda', 
              :source => 'http://gems.github.com', 
-             :version => '>= 2.0.5'
+             :version => '>= 2.0.6'
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
