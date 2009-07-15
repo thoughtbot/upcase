ce036a666900fe427d9b6aee2f0e56d6a969da30
Dan Croak <dcroak@thoughtbot.com>
2009-07-14 22:58:11 -0400

    removed old should_have_form

--- test/shoulda_macros/forms.rb
+++ test/shoulda_macros/forms.rb
@@ -1,31 +1,4 @@
 class Test::Unit::TestCase
-  def self.should_have_form(opts)
-    model = self.name.gsub(/ControllerTest$/, '').singularize.downcase
-    model = model[model.rindex('::')+2..model.size] if model.include?('::')
-    http_method, hidden_http_method = form_http_method opts[:method]
-    should "have a #{model} form" do
-      assert_select "form[action=?][method=#{http_method}]", eval(opts[:action]) do
-        if hidden_http_method
-          assert_select "input[type=hidden][name=_method][value=#{hidden_http_method}]"
-        end
-        opts[:fields].each do |attribute, type|
-          attribute = attribute.is_a?(Symbol) ? "#{model}[#{attribute.to_s}]" : attribute
-          assert_select "input[type=#{type.to_s}][name=?]", attribute
-        end
-        assert_select "input[type=submit]"
-      end
-    end
-  end
-
-  def self.form_http_method(http_method)
-    http_method = http_method.nil? ? 'post' : http_method.to_s
-    if http_method == "post" || http_method == "get"
-      return http_method, nil
-    else
-      return "post", http_method
-    end
-  end
-
   # assert_form posts_url, :put do
   #   assert_text_field :post, :title
   #   assert_text_area  :post, :body
