7102e678759ee9cb55fd04d05bd1d98e3678f262
Matt Jankowski <mjankowski@thoughtbot.com>
2009-08-04 17:27:42 -0400

    remove mile marker

--- vendor/plugins/mile_marker/init.rb
+++
@@ -1,4 +0,0 @@
-require 'mile_marker'
-ActionView::Base.send :include, Thoughtbot::MileMarkerHelper
-ActionController::Base.send :include, Thoughtbot::MileMarkerHelper
-ActionController::Base.send :after_filter, :add_initialize_mile_marker
