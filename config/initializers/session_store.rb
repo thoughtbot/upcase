667b84d5d3435fc469b8c68650cc27c0c8b024db
Matt Jankowski <mjankowski@thoughtbot.com>
2009-08-14 12:12:38 -0400

    rails renamed :session_key to :key in session options hash

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :session_key => "_CHANGEME_session",
-  :secret      => "CHANGESESSION"
+  :key    => "_CHANGEME_session",
+  :secret => "CHANGESESSION"
 }
 
 # Use the database for sessions instead of the cookie-based default,
