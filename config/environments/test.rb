fbdb0fc3df766695fbffedcd10ead3c66d15d747
Mike Burns <mburns@thoughtbot.com>
2009-09-16 10:07:27 -0400

    Use nokogiri-1.3.3

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,7 +40,7 @@ config.gem 'fakeweb',
 # Webrat and dependencies
 # NOTE: don't vendor nokogiri - it's a binary Gem
 config.gem 'nokogiri',
-           :version => '1.3.2',
+           :version => '1.3.3',
            :lib     => false
 config.gem 'webrat',
            :version => '0.4.4'
