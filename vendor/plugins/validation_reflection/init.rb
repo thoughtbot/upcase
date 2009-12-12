f93ae0df16ba634ce592a030a24fbe972a8ee16d
Matt Jankowski <mjankowski@thoughtbot.com>
2009-12-12 15:37:00 -0500

    update validation reflection plugin

--- vendor/plugins/validation_reflection/init.rb
+++
@@ -1,8 +0,0 @@
-
-require 'boiler_plate/validation_reflection'
-
-ActiveRecord::Base.class_eval do
-  include BoilerPlate::ActiveRecordExtensions::ValidationReflection
-  BoilerPlate::ActiveRecordExtensions::ValidationReflection.load_config
-  BoilerPlate::ActiveRecordExtensions::ValidationReflection.install(self)
-end
