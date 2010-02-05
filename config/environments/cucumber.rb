0bd323eb6d0a354dbbc94c236b8e83930dfa24b8
Matt Jankowski <mjankowski@thoughtbot.com>
2010-02-05 10:40:29 -0500

    call out cucumber reqs

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -21,11 +21,14 @@ config.action_controller.allow_forgery_protection    = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-
 config.gem 'factory_girl', 
            :version => '>= 1.2.3'
 
 # Cucumber and dependencies
+config.gem 'cucumber-rails',   :lib => false, :version => '>=0.2.4' unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber-rails'))
+config.gem 'database_cleaner', :lib => false, :version => '>=0.4.3' unless File.directory?(File.join(Rails.root, 'vendor/plugins/database_cleaner'))
+config.gem 'webrat',           :lib => false, :version => '>=0.7.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/webrat'))
+
 config.gem 'polyglot',
            :version => '0.2.9',
            :lib     => false
@@ -41,20 +44,11 @@ config.gem 'diff-lcs',
 config.gem 'builder',
            :version => '2.1.2',
            :lib     => false
-config.gem 'cucumber',
-           :version => '>= 0.6.2'
-config.gem 'cucumber-rails',
-           :version => '>= 0.2.4',
-           :lib => false
-config.gem 'database_cleaner',
-           :version => '>= 0.4.3'
 
 # Webrat and dependencies
 # NOTE: don't vendor nokogiri - it's a binary Gem
 config.gem 'nokogiri',
            :version => '>= 1.4.0',
            :lib     => false
-config.gem 'webrat',
-           :version => '0.7.0'
 
 HOST = 'localhost'
