3782ac2419e6581794301c936dba9534d01cce35
Matthew Mongeau <matt@toastyapps.com>
2010-08-05 16:43:23 -0400

    2.3.8 session fix. Vendor correct version of Rack

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -52,3 +52,21 @@ config.gem 'nokogiri',
            :lib     => false
 
 HOST = 'localhost'
+
+class RackRailsCookieHeaderHack
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    status, headers, body = @app.call(env)
+    if headers['Set-Cookie'] && headers['Set-Cookie'].respond_to?(:collect!)
+      headers['Set-Cookie'].collect! { |h| h.strip }
+    end
+    [status, headers, body]
+  end
+end
+
+config.after_initialize do
+  ActionController::Dispatcher.middleware.insert_before(ActionController::Base.session_store, RackRailsCookieHeaderHack)
+end
