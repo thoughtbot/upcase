ecd3b321a77c51682b4e6f6e8d91c4290d73daa5
Mike Burns <mburns@thoughtbot.com>
2010-09-07 16:01:30 -0400

    Adding a course with more details

--- features/step_definitions/form_steps.rb
+++ features/step_definitions/form_steps.rb
@@ -5,3 +5,7 @@ end
 When 'I press the button to $button_action' do |button_action|
   click_button button_text_for(button_action)
 end
+
+When 'I check $check_box_description' do |check_box_description|
+  check field_id_for(check_box_description)
+end
