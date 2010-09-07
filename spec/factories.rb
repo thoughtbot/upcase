28a4923232351e9cc4f7b0124842d939dd3f2a23
Mike Burns <mburns@thoughtbot.com>
2010-09-07 16:17:16 -0400

    Validations on a course

--- spec/factories.rb
+++ spec/factories.rb
@@ -17,6 +17,12 @@ Factory.define :admin, :parent => :email_confirmed_user do |user_factory|
 end
 
 Factory.define(:course) do |course_factory|
+  course_factory.name        "Test-Driven Prolog"
+  course_factory.description "Solve 8-Queens over and over again"
+  course_factory.price       "500"
+  course_factory.start_at    '9:00'
+  course_factory.stop_at     '17:00'
+  course_factory.location    '41 Winter St Boston, MA'
 end
 
 Factory.define(:section) do |section_factory|
