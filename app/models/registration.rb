59cecdf4ec23389ce782bb701173a181a2ba6825
Matthew Mongeau <matt@toastyapps.com>
2010-09-16 11:26:47 -0400

    Registering an existing student

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,4 +3,13 @@ class Registration < ActiveRecord::Base
   belongs_to :user
 
   validates_associated :user
+
+  def assign_user(user_params)
+    if User.exists?(:email => user_params[:email])
+      self.user = User.find_by_email(user_params[:email])
+    else
+      self.user = build_user(user_params.
+                             merge(:password => '12343', :password_confirmation => '12343'))
+    end
+  end
 end
