fb7f87a122129844721a27b5593805d5f5c0525b
Joe Ferris <joe.r.ferris@gmail.com>
2010-09-17 12:30:05 -0400

    Admins can add resources to a course

--- features/step_definitions/course_html_steps.rb
+++ features/step_definitions/course_html_steps.rb
@@ -43,5 +43,25 @@ Then 'I see the following questions:' do |question_table|
   end
 end
 
+When 'I fill in the following resources:' do |resource_table|
+  resource_table.hashes.each_with_index do |resource_hash, index|
+    number = index + 1
+    steps %{
+      When I fill in the resource #{number} field with "#{resource_hash['name']}"
+      And I fill in the url #{number} field with "#{resource_hash['url']}"
+    }
+  end
+end
+
+Then 'I see the following resources:' do |resource_table|
+  resource_table.hashes.each_with_index do |resource_hash, index|
+    number = index + 1
+    steps %{
+      Then the resource #{number} field should contain "#{resource_hash['name']}"
+      And the url #{number} field should contain "#{resource_hash['url']}"
+    }
+  end
+end
+
 World(ActionController::RecordIdentifier)
 
