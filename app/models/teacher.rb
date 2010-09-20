034c131c68e486bf1528f6ccd8bc1739cf653105
Matthew Mongeau <matt@toastyapps.com>
2010-09-20 17:09:31 -0400

    Display teacher information

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,6 +2,10 @@ class Teacher < ActiveRecord::Base
   before_save :set_gravatar_hash
   validates_presence_of :name, :email
 
+  def image_name
+    (name[0,1]+name.split(" ")[1]).downcase
+  end
+
   protected
 
   def set_gravatar_hash
