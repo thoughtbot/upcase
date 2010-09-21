15c0e786ebec363f5c48c1636bffc6fb9dbd47cc
Matthew Mongeau <matt@toastyapps.com>
2010-09-21 14:14:44 -0400

    Link the map link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
   def format_date_range(range)
     range
   end
+
+  def google_map_link_to(address, *options, &block)
+    google_link = "http://maps.google.com/maps?f=q&q=#{CGI.escape(address)}&z=17&iwloc=A"
+    if block_given?
+      concat(link_to(capture(&block), google_link, *options))
+    else
+      link_to address, google_link, *options
+    end
+  end
 end
