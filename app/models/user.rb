fe451f9f94f41364c89ee75710a18e89b6853b52
Matthew Mongeau <matt@toastyapps.com>
2010-09-22 13:45:00 -0400

    Send set password email. Removed ability to sign up without registering for a course... for now.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,19 @@ class User < ActiveRecord::Base
   has_many :registrations
   has_many :sections, :through => :registrations
 
+  after_create :send_set_password_email
+
   def registered_for?(section)
     sections.include?(section)
   end
 
   def name
     [first_name, last_name].join(' ')
+  end
+
+  private
+
+  def send_set_password_email
+    UserMailer.deliver_set_password(self)
   end
 end
