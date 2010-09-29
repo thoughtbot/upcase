c21be38cd4ce97105e554251a1f53e26db0731a0
Matthew Mongeau <matt@toastyapps.com>
2010-09-29 12:26:07 -0400

    Switch to gravatars on section page.

--- app/helpers/teacher_helper.rb
+++ app/helpers/teacher_helper.rb
@@ -1,7 +1,11 @@
 module TeacherHelper
   def teacher_label
     Proc.new do |teacher|
-      image_tag("http://www.gravatar.com/avatar/#{teacher.gravatar_hash}?s=20")+h(teacher.name)
+      teacher_gravatar(teacher)+h(teacher.name)
     end
+  end
+
+  def teacher_gravatar(teacher)
+    image_tag("http://www.gravatar.com/avatar/#{teacher.gravatar_hash}?s=20", :alt => teacher.name)
   end
 end
