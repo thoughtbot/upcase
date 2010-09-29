6040cd7572a4c717d26763413bea19cd87c3c8f7
Matthew Mongeau <matt@toastyapps.com>
2010-09-29 14:30:21 -0400

    Different environments should use different chargify URLs

--- config/initializers/chargify.rb
+++ config/initializers/chargify.rb
@@ -1,1 +1,8 @@
-CHARGIFY_API_KEY = "29fGw6QMQGJCtPGO2nW-"
+if Rails.env.production?
+  CHARGIFY_API_KEY = "29fGw6QMQGJCtPGO2nW-"
+  CHARGIFY_URL = "thoughtbot-workshops.chargify.com"
+else
+  CHARGIFY_API_KEY = "29fGw6QMQGJCtPGO2nW-"
+  CHARGIFY_URL = "thoughtbot-workshops-staging.chargify.com"
+end
+
