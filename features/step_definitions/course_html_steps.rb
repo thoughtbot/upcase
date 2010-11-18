909ff15d7be4028d7d9f852391f983e82535e29b
Matthew Mongeau <matt@toastyapps.com>
2010-11-18 14:57:46 -0500

    Do not show resources or faq if none exist. Sort sections.

--- features/step_definitions/course_html_steps.rb
+++ features/step_definitions/course_html_steps.rb
@@ -1,6 +1,6 @@
 Then 'I see the course resource page for "$course_name"' do |course_name|
   page.should have_content(course_name)
-  page.should have_content('Resources')
+  page.should have_no_content('Register for')
 end
 
 Then 'I see the admin listing include a course named "$course_name"' do |course_name|
