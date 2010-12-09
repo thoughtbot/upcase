d2ea57b860b8ee91319fda2f995e774fa3e78b75
Matthew Mongeau <halogenandtoast@gmail.com>
2010-12-09 11:05:00 -0500

    Validate email address for follow ups

--- app/models/follow_up.rb
+++ app/models/follow_up.rb
@@ -1,4 +1,5 @@
 class FollowUp < ActiveRecord::Base
   belongs_to :course
   validates_presence_of :email
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
