e6a6070a554cf3f96693de0e5bd17ce9b648f03f
Chad Pytel <cpytel@thoughtbot.com>
2011-03-31 16:49:53 -0400

    short description on courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-  validates_presence_of :name, :description, :price, :start_at, :stop_at, :location, :maximum_students
+  validates_presence_of :name, :description, :price, :start_at, :stop_at, :location, :maximum_students, :short_description
 
   has_many :sections
   has_many :questions
@@ -10,7 +10,6 @@ class Course < ActiveRecord::Base
   accepts_nested_attributes_for :follow_ups, :reject_if => :all_blank
 
   named_scope :unscheduled, lambda { { :conditions => ["courses.id not in (select sections.course_id from sections where sections.ends_on >= ?)", Date.today] } }
-
 
   def questions_with_blank
     questions + [questions.new]
