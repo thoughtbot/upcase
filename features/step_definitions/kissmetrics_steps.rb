8c7f32e96d92e7e5d24cd8580f7a7d61c8efb9aa
Jason Morrison <jmorrison@thoughtbot.com>
2011-07-01 11:36:38 -0400

    Add step definitions for KISSmetrics

---
+++ features/step_definitions/kissmetrics_steps.rb
@@ -0,0 +1,23 @@
+Then /^KISSmetrics receives the "([^"]*)" event with:$/ do |event_name, table|
+  properties = table.rows_hash
+
+  if using_javascript_driver?
+    kmq = JSON.parse(page.evaluate_script("JSON.stringify(_kmq)"))
+    matched_event = kmq.detect { |call| call == ["record", event_name, properties] }
+    matched_event.should be
+  else
+    expected_javascript = %Q{_kmq.push(["record","#{event_name}",#{properties.to_json}]);}
+    page.should have_content(expected_javascript)
+  end
+end
+
+Then /^KISSmetrics does not receive the "([^"]*)" event$/ do |event_name|
+  if using_javascript_driver?
+    kmq = JSON.parse(page.evaluate_script("JSON.stringify(_kmq)"))
+    matched_event = kmq.detect { |call| call == ["record", event_name, properties] }
+    matched_event.should be_nil
+  else
+    unexpected_javascript = %Q{_kmq.push(["record","#{event_name}"}
+    page.should have_no_content(unexpected_javascript)
+  end
+end
