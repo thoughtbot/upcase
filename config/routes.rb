a1cdef2926885454e878aa24df43f6d561789ce5
Chad Mazzola <cmazzola@thoughtbot.com>
2011-07-14 14:26:53 -0400

    Further work on design refresh. Add backbone page and modify route.

--- config/routes.rb
+++ config/routes.rb
@@ -30,10 +30,11 @@ Workshops::Application.routes.draw do
   match '/fairhaven/registered' => 'high_voltage/pages#show', :as => :fairhaven_registered, :id => 'fairhaven-registered'
   match '/fairhaven/resources' => 'high_voltage/pages#show', :as => :fairhaven_resources, :id => 'fairhaven-resources'
 
-
   match '/intro-rails' => "high_voltage/pages#show", :as => :intro_rails, :id => "intro-rails"
   match '/everybody-codes' => "high_voltage/pages#show", :as => :everybody_codes, :id => "everybody-codes"
 
   match '/directions' => "high_voltage/pages#show", :as => :directions, :id => "directions"
   match '/group-training' => "high_voltage/pages#show", :as => :group_training, :id => "group-training"
+
+  match '/backbone-on-rails' => "high_voltage/pages#show", :as => :backbone_on_rails, :id => "backbone-on-rails"
 end
