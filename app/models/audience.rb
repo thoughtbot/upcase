96e30ec471c96186959e9ec117db61564bb3d142
Dan Croak <dcroak@thoughtbot.com>
2011-08-12 17:55:13 -0400

    display courses on homepage grouped by audience

--- app/models/audience.rb
+++ app/models/audience.rb
@@ -1,5 +1,9 @@
 class Audience < ActiveRecord::Base
-  has_many :courses
+  has_many :courses, :order => "courses.position asc"
 
   validates_presence_of :name
+
+  def self.by_position
+    order("audiences.position asc")
+  end
 end
