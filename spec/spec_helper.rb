8edf1bd75f9952869ee859ae8d0ab5536911ef84
Matt Jankowski <mjankowski@thoughtbot.com>
2011-08-18 11:15:56 -0400

    more rspec deprecation updates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ Mocha::Configuration.warn_when(:stubbing_non_public_method)
 # in ./support/ and its subdirectories.
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   # If you're not using ActiveRecord you should remove these
   # lines, delete config/database.yml and disable :active_record
   # in your config/boot.rb
