5c591aa7944acb45b6782bbd60faa6f52feadd80
Prem Sichanugrist <s@sikachu.com>
2011-09-14 23:17:39 +0700

    Do not update #notified_at column for the old records

--- db/migrate/20110914152250_add_notified_at_to_follow_ups.rb
+++ db/migrate/20110914152250_add_notified_at_to_follow_ups.rb
@@ -1,7 +1,6 @@
 class AddNotifiedAtToFollowUps < ActiveRecord::Migration
   def self.up
     add_column :follow_ups, :notified_at, :datetime
-    connection.update_sql("UPDATE follow_ups SET notified_at='#{Time.now.utc}'")
   end
 
   def self.down
