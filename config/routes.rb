ad73926a686cf6f26fe5ced0576c5d131fa19006
Chad Pytel <cpytel@thoughtbot.com>
2012-02-03 22:39:34 -0500

    playbook page

--- config/routes.rb
+++ config/routes.rb
@@ -44,4 +44,5 @@ Workshops::Application.routes.draw do
   match '/charlotte' => "high_voltage/pages#show", :as => :charlotte, :id => "charlotte"
 
   match '/rubyist-booster-shot' => "high_voltage/pages#show", :as => :rubyist_booster_shot, :id => "rubyist-booster-shot"
+  match '/playbook' => "high_voltage/pages#show", :as => :playbook, :id => "playbook"
 end
