8183c588373ce4f35761f0845fcf16b5530476fd
Chad Pytel <cpytel@thoughtbot.com>
2012-02-13 13:31:46 -0500

    Finishes https://thoughtbot.apptrajectory.com/thoughtbot/workshops/stories/114393 Squashed commit of the following:
    commit 7a3a63940e52302fa357b49623ab4b7549cfe403
    Author: Chad Pytel <cpytel@thoughtbot.com>
    Date:   Mon Feb 13 11:25:30 2012 -0500
    
        add style and correct markup for backbone checkout
    
    commit a3b512bb8b0bebb917146773d8f834b644cf75fa
    Author: Chad Pytel <cpytel@thoughtbot.com>
    Date:   Mon Feb 13 10:36:23 2012 -0500
    
        add some covoerage for shopify order_paid controller
    
    commit 585b5de180bd94092a71451de3bbf6cf07d75049
    Author: Chad Pytel <cpytel@thoughtbot.com>
    Date:   Sun Feb 12 22:32:38 2012 -0500
    
        group checkout page
    
    commit be972d2d43d14c90717938cab676f73bfe7d3180
    Author: Chad Pytel <cpytel@thoughtbot.com>
    Date:   Sun Feb 12 22:15:55 2012 -0500
    
        use password for auth
    
    commit 84740effd755952a09b49a95a74110c90ad2ccb6
    Author: Chad Pytel <cpytel@thoughtbot.com>
    Date:   Sun Feb 12 22:02:28 2012 -0500
    
        use github client differently to try to get it to work
    
    commit add266429aa7f166c584e98559c2dcdeb632a3ad
    Author: Chad Pytel <cpytel@thoughtbot.com>
    Date:   Sun Feb 12 21:52:45 2012 -0500
    
        add reader to github
    
    commit 364cde6c068aa493178b7c37f5d7a7ed51cd8de1
    Author: Chad Pytel <cpytel@thoughtbot.com>
    Date:   Sun Feb 12 21:14:51 2012 -0500
    
        specify shopify controller
    
    commit 545bb27864115023aa8d92d81ec5f4319669f731
    Author: Chad Pytel <cpytel@thoughtbot.com>
    Date:   Sun Feb 12 21:12:24 2012 -0500
    
        member route instead
    
    commit ad0db5e853fa7c80133c360809481af445995ca3
    Author: Chad Pytel <cpytel@thoughtbot.com>
    Date:   Sun Feb 12 21:11:46 2012 -0500
    
        proper route to shopify controller order_paid
    
    commit b191778e0d928f777091055446d9b7dcb261e2ae
    Author: Chad Pytel <cpytel@thoughtbot.com>
    Date:   Sun Feb 12 21:05:12 2012 -0500
    
        introduce shopify controller

---
+++ spec/controllers/shopify_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe ShopifyController do
+  it "doesn't add any users to github when it is not a backbone book sale" do
+    client = stub(:add_team_member => nil)
+    Octokit::Client.stubs(:new => client)
+    post :order_paid, "note_attributes"=>[{"name"=>"first-reader", "value"=>"cpytel"}], "line_items"=>[{"id"=>198671572, "product_id"=>84369372, "quantity"=>1, "sku"=>"NOTBACKBONE"}]
+    client.should_not have_received(:add_team_member)
+  end
+
+  it "adds any users to github when it is a backbone book sale" do
+    client = stub(:add_team_member => nil)
+    Octokit::Client.stubs(:new => client)
+    post :order_paid, "note_attributes"=>[{"name"=>"first-reader", "value"=>"cpytel"}], "line_items"=>[{"id"=>198671572, "product_id"=>84369372, "quantity"=>1, "sku"=>"BACKBONE"}]
+    client.should have_received(:add_team_member).with(73110, "cpytel")
+  end
+
+  it "adds multiple users to github when it is a backbone book sale" do
+    client = stub(:add_team_member => nil)
+    Octokit::Client.stubs(:new => client)
+    post :order_paid, "note_attributes"=>[{"name"=>"first-reader", "value"=>"cpytel"}, {"name"=>"second-reader", "value"=>"reader2"}], "line_items"=>[{"id"=>198671572, "product_id"=>84369372, "quantity"=>1, "sku"=>"BACKBONE"}]
+    client.should have_received(:add_team_member).with(73110, "cpytel")
+    client.should have_received(:add_team_member).with(73110, "reader2")
+  end
+
+end
