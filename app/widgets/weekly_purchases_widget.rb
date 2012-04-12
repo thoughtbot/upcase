96adfd341ec373323c77cad4957a11e858460f6f
Chad Pytel <cpytel@thoughtbot.com>
2012-04-12 15:34:58 -0400

    weekly purchases

---
+++ app/widgets/weekly_purchases_widget.rb
@@ -0,0 +1,11 @@
+widget :weekly_purchases do
+  key "2019ad43df333537c31efef1ff7d73bfe8178240"
+  type "number_and_secondary"
+  data do
+    {
+      :value => Purchase.from_period(7.days.ago, Time.now),
+      :previous => Purchase.from_period(14.days.ago, 7.days.ago),
+      :prefix => "$"
+    }
+  end
+end
