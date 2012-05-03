3f45e92fffaf22d2e791c0f78d60b871d9199056
Chad Pytel <cpytel@thoughtbot.com>
2012-05-02 23:28:44 -0700

    use time instead of date to gets times

--- app/controllers/admin/purchases_controller.rb
+++ app/controllers/admin/purchases_controller.rb
@@ -3,7 +3,7 @@ class Admin::PurchasesController < AdminController
     respond_to do |format|
       format.html
       format.csv do
-        @purchases = Purchase.from_month(Date.parse(params[:date]))
+        @purchases = Purchase.from_month(Time.parse(params[:date]))
         render :csv => @purchases, :fields => [:id, :name, :email, :paid, :payment_method, :payment_transaction_id, :product_name, :price, :created_at]
       end
     end
