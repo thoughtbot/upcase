7d0935fded75244ab8ff01012bbb2ec4bfcd611a
Ben Orenstein <ben.orenstein@gmail.com>
2012-05-04 11:29:38 -0400

    Set up infrastructure to A/B test the chrome screencast description.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,6 +2,7 @@ class PurchasesController < ApplicationController
   def new
     @product = Product.find(params[:product_id])
     @purchase = @product.purchases.build(:variant => params[:variant])
+    track_chrome_screencast_ab_test_completion
   end
 
   def create
@@ -30,5 +31,13 @@ class PurchasesController < ApplicationController
     @purchase = @product.purchases.find_by_lookup!(params[:id])
     @purchase.complete_paypal_payment!(params[:token], params[:PayerID])
     redirect_to product_purchase_path(@purchase.product, @purchase) 
+  end
+
+  private
+
+  def track_chrome_screencast_ab_test_completion
+    if @product.name == "Hidden Secrets of the Chrome Developer Tools"
+      finished('new_chrome_cast_description')
+    end
   end
 end
