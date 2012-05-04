dbafc550e03375d1922e09e6975c8f1316de1881
Ben Orenstein <ben.orenstein@gmail.com>
2012-05-04 11:29:38 -0400

    Set up infrastructure to A/B test the chrome screencast description.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,17 @@
 class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
+
+    ab_test_chrome_screencast
+  end
+
+  private
+
+  def ab_test_chrome_screencast
+    if @product.name == "Hidden Secrets of the Chrome Developer Tools"
+      if ab_test('new_chrome_cast_description', 'original', 'alternative') == 'alternative'
+        render 'alternative'
+      end
+    end
   end
 end
