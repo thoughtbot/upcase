8b02404aab55db0b135c0c5df5aeeae26ee97c31
Jason Draper <jason@drapergeek.com>
2012-05-16 15:10:19 -0400

    Add tests for the registration model

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -18,4 +18,47 @@ describe Registration do
     should_not allow_value('chad').for(:email)
     should_not allow_value('chad@blah').for(:email)
   end
+
+  it "forms a name from first and last name" do
+    registration = build(:registration, first_name: "Bruce", last_name: "Banner")
+    registration.name.should == "Bruce Banner"
+  end
+
+  it "can receive a payment" do
+    registration = build_stubbed(:registration, paid: false)
+    registration.expects("send_payment_confirmations")
+    registration.receive_payment!
+    registration.should be_paid
+    registration.should_not be_changed
+  end
+
+  it "delegates to section for course price" do
+    registration = build_stubbed(:registration)
+    registration.section.expects("course_price")
+    registration.section_price
+  end
+
+  it "delgates to coupon for price when a coupon is applied" do
+    coupon = build_stubbed(:coupon)
+    registration = build_stubbed(:registration, coupon: coupon)
+    coupon.expects("apply").with(registration.section_price)
+    registration.price
+  end
+
+  it "returns the section price when no coupon is applied" do
+    registration = build_stubbed(:registration)
+    registration.expects("section_price")
+    registration.price
+  end
+
+  it "uses the freshbooks_invoice_url is available" do
+    registration = build_stubbed(:registration, freshbooks_invoice_url: "https://example.com")
+    registration.freshbooks_invoice_url.should == "https://example.com"
+  end
+
+  it "uses the fetch_invoiceurl for freshbooks_invoice_url if one is not available" do
+    registration = build_stubbed(:registration, freshbooks_invoice_url: nil)
+    registration.expects("fetch_invoice_url")
+    registration.freshbooks_invoice_url
+  end
 end
