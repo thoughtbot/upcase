a83dd4facd4d2af2f8ed7ac5920ca20f94e375f5
Ben Zhang <bzhang@thoughtbot.com>
2012-05-21 14:17:31 -0400

    updated embedded step syntax

--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -1,14 +1,14 @@
 Given 'I am signed in as "$email"' do |email|
   user = create(:user, email: email)
-  Given %{I sign in as "#{user.email}"}
+  step %{I sign in as "#{user.email}"}
 end
 
 Given 'I am signed in as an admin' do
   user = create(:admin)
-  Given %{I sign in as "#{user.email}"}
+  step %{I sign in as "#{user.email}"}
 end
 
 Given 'I am signed in as a student' do
   user = create(:user)
-  Given %{I sign in as "#{user.email}"}
+  step %{I sign in as "#{user.email}"}
 end
