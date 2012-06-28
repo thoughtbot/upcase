fdab3ca93f5acaf0fa4eaaa3d6a04dfe4d2ba8e4
Chad Pytel <cpytel@thoughtbot.com>
2012-06-28 11:21:23 -0400

    allow non-vcr net connections

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -2,6 +2,7 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.configure_rspec_metadata!
+  config.allow_http_connections_when_no_cassette = true
   config.preserve_exact_body_bytes { true }
   config.default_cassette_options = {
     re_record_interval: 1.week
