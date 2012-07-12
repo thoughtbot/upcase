a452f25c755493096a41b520f5f7a347048d1afe
Chad Pytel <cpytel@thoughtbot.com>
2012-07-11 21:39:58 -0400

    only show top topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,7 +3,7 @@ class TopicsController < ApplicationController
     @books = Product.books.active
     @screencasts = Product.screencasts.active
     @courses = Course.only_public.by_position
-    @topics = Topic.all
+    @topics = Topic.top
     @articles = Article.all
   end
 
@@ -12,7 +12,7 @@ class TopicsController < ApplicationController
     @books = Product.books.for_topic(@topic).active
     @screencasts = Product.screencasts.for_topic(@topic).active
     @courses = Course.for_topic(@topic).only_public.by_position
-    @topics = Topic.all
+    @topics = Topic.top
     @articles = @topic.articles
     render :index
   end
