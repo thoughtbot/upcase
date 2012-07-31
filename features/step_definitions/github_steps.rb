7a07b10044ca6e51ce5a761d7d64807e66b67574
Mike Burns <mike@mike-burns.com>
2012-07-31 14:57:31 -0400

    Update the hardcoded password in the GitHub stub

--- features/step_definitions/github_steps.rb
+++ features/step_definitions/github_steps.rb
@@ -1,3 +1,3 @@
 Given /^github is stubbed$/ do
-  stub_request(:put, 'https://cpytel:cambridge@api.github.com/teams/members/thoughtbot').with(:body => '{"name":"thoughtbot"}', :headers => {'Accept'=>'*/*', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).to_return(:status => 200, :body => "", :headers => {})
+  stub_request(:put, 'https://cpytel:eqZUjxaaaqk33ob@api.github.com/teams/members/thoughtbot').with(:body => '{"name":"thoughtbot"}', :headers => {'Accept'=>'*/*', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).to_return(:status => 200, :body => "", :headers => {})
 end
