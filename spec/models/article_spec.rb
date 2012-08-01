ab32f0326e534e89d17653a5b34adc2175304b8c
Chad Pytel <cpytel@thoughtbot.com>
2012-08-01 11:52:34 -0400

    remove attr_accessible specs on article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,14 +7,6 @@ describe Article do
     it { should have_many(:topics).through(:classifications) }
   end
 
-  context 'active_authorizer' do
-    it { should_not allow_mass_assignment_of(:created_at) }
-    it { should_not allow_mass_assignment_of(:body_html) }
-    it { should_not allow_mass_assignment_of(:title) }
-    it { should_not allow_mass_assignment_of(:tumblr_url) }
-    it { should_not allow_mass_assignment_of(:updated_at) }
-  end
-
   context 'validations' do
     it { should validate_presence_of(:body_html) }
     it { should validate_presence_of(:published_on) }
