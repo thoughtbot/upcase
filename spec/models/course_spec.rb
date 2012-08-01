788ebded43a70d9da90faee992e25a6d30d2c996
Joel Oliveira <joel@thoughtbot.com>
2012-08-01 16:34:57 -0400

    Change `FactoryGirl.create` to just `create`

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -16,10 +16,10 @@ describe Course do
 
   describe '#find_all_courses_or_by_topics' do
     it 'includes courses for the given topics' do
-      topic_1 = FactoryGirl.create(:topic, name: 'ruby')
-      topic_2 = FactoryGirl.create(:topic, name: 'rubygems')
-      course = FactoryGirl.create(:course, public: true)
-      course_not_in_topics = FactoryGirl.create(:course)
+      topic_1 = create(:topic, name: 'ruby')
+      topic_2 = create(:topic, name: 'rubygems')
+      course = create(:course, public: true)
+      course_not_in_topics = create(:course)
 
       found_topics = [topic_1, topic_2]
       found_topics.each { |topic| topic.courses << course }
@@ -29,9 +29,9 @@ describe Course do
     end
 
     it 'includes all courses if no topics are found' do
-      course_1 = FactoryGirl.create(:course, public: true)
-      course_2 = FactoryGirl.create(:course, public: true)
-      found_topics = [FactoryGirl.create(:topic)]
+      course_1 = create(:course, public: true)
+      course_2 = create(:course, public: true)
+      found_topics = [create(:topic)]
       Course.find_all_courses_or_by_topics(found_topics).should include course_1
       Course.find_all_courses_or_by_topics(found_topics).should include course_2
     end
@@ -39,10 +39,10 @@ describe Course do
 
   describe "#for_topic" do
     it "includes only courses for the given topic" do
-      topic = FactoryGirl.create(:topic)
-      in_topic = FactoryGirl.create(:course)
+      topic = create(:topic)
+      in_topic = create(:course)
       topic.courses << in_topic
-      not_in_topic = FactoryGirl.create(:course)
+      not_in_topic = create(:course)
 
       Course.for_topic(topic).should include in_topic
       Course.for_topic(topic).should_not include not_in_topic
