799b12b09a8a46b6f2d91587af9d64a58d2a2941
Chad Pytel <cpytel@thoughtbot.com>
2012-08-03 15:15:34 -0400

    fix rack-rewrite hostname redirect

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
 
   Rails.configuration.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
     r301 %r{.*}, "http://#{HOST}$&", if: Proc.new { |rack_env|
-      rack_env['SERVER_NAME'] != "HOST"
+      rack_env['SERVER_NAME'] != "#{HOST}"
     }
   end
 end
