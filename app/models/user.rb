96cae842f503fbd3d0f7f6f614f4240d86068b61
Chad Pytel <cpytel@thoughtbot.com>
2012-08-06 15:34:38 -0400

    purchases associated with users on creation
    wip: purchases associated with users on creation
    
    associate previous purchases with new users with the same email
    
    use the previous stripe_customer  on new users if one exists

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,21 @@ class User < ActiveRecord::Base
 
   attr_accessible :last_name, :password, :email, :first_name
 
+  has_many :purchases
+
   validates_presence_of :first_name, :last_name
+
+  after_create :associate_previous_purchases
 
   def name
     [first_name, last_name].join(' ')
+  end
+
+  private
+
+  def associate_previous_purchases
+    previous_purchases = Purchase.by_email(email)
+    self.purchases << previous_purchases
+    self.update_column(:stripe_customer, previous_purchases.stripe.last.try(:stripe_customer))
   end
 end
