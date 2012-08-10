9a4b3c3495a1b58a758cbaba3fbdd9a6af4ab0bb
Joel Oliveira <joel@thoughtbot.com>
2012-08-10 16:32:46 -0400

    Removing the conversion of wistia_hash to JSON.
    * Remove all related JSON.parse's as well

--- app/models/video.rb
+++ app/models/video.rb
@@ -6,26 +6,26 @@ class Video < ActiveRecord::Base
   validates_presence_of :product_id, :wistia_id
 
   def video_sizes
-    @video_sizes ||= JSON.parse(wistia_hash)["assets"].inject({}){|result, asset| result.merge(asset["type"]=>human_file_size(asset['fileSize']))}
+    @video_sizes ||= wistia_hash["assets"].inject({}){|result, asset| result.merge(asset["type"]=>human_file_size(asset['fileSize']))}
   rescue
     {}
   end
 
   def video_hash_id
-    @video_hash_id ||= JSON.parse(wistia_hash)["hashed_id"] rescue nil
+    @video_hash_id ||= wistia_hash["hashed_id"] rescue nil
   end
 
   def wistia_hash
-    @wistia_hash ||= Wistia.get_media_hash_from_id(wistia_id).to_json unless wistia_id.nil?
-  end
-
-  def wistia_thumbnail
-    @wistia_thumbnail ||= JSON.parse(wistia_hash)["thumbnail"]["url"] rescue nil
+    @wistia_hash ||= Wistia.get_media_hash_from_id(wistia_id) unless wistia_id.nil?
   end
 
   def wistia_running_time
-    @wistia_running_time ||= JSON.parse(wistia_hash)["duration"] rescue nil
+    @wistia_running_time ||= wistia_hash["duration"] rescue nil
     Time.at(@wistia_running_time.to_i).gmtime.strftime('%M:%S')
+  end
+
+  def wistia_thumbnail
+    @wistia_thumbnail ||= wistia_hash["thumbnail"]["url"] rescue nil
   end
 
   private
