ea938651b89bac5b003fcd0fb42810b76054f976
Joel Oliveira <joel@thoughtbot.com>
2012-08-10 16:32:46 -0400

    Removing the conversion of wistia_hash to JSON.
    * Remove all related JSON.parse's as well

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,7 +4,7 @@ describe Video do
   context '#wistia_thumbnail' do
     it 'returns the thumbnail cached from wistia' do
       video = build_stubbed(:video,
-        wistia_hash: { thumbnail: { url: 'http://images.com/hi.jpg' } }.to_json)
+        wistia_hash: { 'thumbnail' => { 'url' => 'http://images.com/hi.jpg' } })
       video.wistia_thumbnail.should == 'http://images.com/hi.jpg'
     end
   end
@@ -12,7 +12,7 @@ describe Video do
   context '#wistia_running_time' do
     it "converts wistia's duration in seconds to running time in MM:SS" do
       video = build_stubbed(:video,
-        wistia_hash: { duration: '2052.97' }.to_json)
+        wistia_hash: { 'duration' => '2052.97' })
       video.wistia_running_time.should == '34:12'
     end
   end
