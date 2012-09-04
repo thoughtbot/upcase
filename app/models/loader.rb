466b5ea350e214a0016889e2e742ddaa9e21b1ba
Ben Zhang <bzhang@thoughtbot.com>
2012-09-04 17:27:41 -0400

    add test for skipping this week in open source content

--- app/models/loader.rb
+++ app/models/loader.rb
@@ -1,14 +1,13 @@
 class Loader
   def self.import_articles_and_topics(posts)
     posts.each do |post|
-      next if post["tags"].include?("this week in open source")
+      next if post[:tags].include?("this week in open source")
       article = Article.find_or_initialize_by_title_and_published_on(post[:title],post[:published_at])
       unless article.persisted?
         article.body_html = post[:body_html]
         article.tumblr_url = post[:tumblr_url]
         article.save!
         post[:tags].each do |tag|
-          puts tag.downcase
           topic = Topic.find_by_slug(tag.downcase.parameterize)
           if topic.nil?
             topic = Topic.new
