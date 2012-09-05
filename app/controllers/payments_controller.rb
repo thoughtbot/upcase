4b150ded7a7b6f5b0fd474388a062500ef0833c7
Chad Pytel <cpytel@thoughtbot.com>
2012-09-05 15:42:58 -0400

    more temporary logging

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -28,6 +28,7 @@ class PaymentsController < ApplicationController
   end
 
   def verify_callback
+    p params
     freshbooks_client.callback.verify(callback_id: params[:callback_id], verifier: params[:verifier])
     head :created
   end
