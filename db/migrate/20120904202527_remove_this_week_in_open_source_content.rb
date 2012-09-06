c09d58e4b6b96048957416d155cc0558c4ed7759
Ben Zhang <bzhang@thoughtbot.com>
2012-09-06 11:24:51 -0400

    change activerecord code to sql code for the migration to work all the time

--- db/migrate/20120904202527_remove_this_week_in_open_source_content.rb
+++ db/migrate/20120904202527_remove_this_week_in_open_source_content.rb
@@ -1,10 +1,25 @@
 class RemoveThisWeekInOpenSourceContent < ActiveRecord::Migration
   def up
-    @topic = Topic.find_by_name("this week in open source")
-    if @topic
-      @topic.articles.destroy_all
-      @topic.destroy
-    end
+    execute <<-SQL
+      DELETE FROM "articles"
+      WHERE "articles"."id" IN (
+        SELECT "articles"."id"
+        FROM "articles" INNER JOIN "classifications" ON "classifications"."classifiable_id" = "articles"."id" AND "classifications"."classifiable_type" = 'Article'
+        INNER JOIN "topics" ON "topics"."id" = "classifications"."topic_id"
+        WHERE (topics.name LIKE 'this week in open source')
+      );
+
+      DELETE FROM "classifications"
+      WHERE "classifications"."id" IN (
+        SELECT "classifications"."id"
+        FROM "classifications"
+        INNER JOIN "topics" ON "topics"."id" = "classifications"."topic_id"
+        WHERE (topics.name LIKE 'this week in open source')
+      );
+
+      DELETE FROM "topics"
+      WHERE "topics"."name" LIKE 'this week in open source';
+    SQL
   end
 
   def down
