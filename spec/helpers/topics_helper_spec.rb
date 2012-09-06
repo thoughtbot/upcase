66aa4cefecb9765dd9dfba323c345840a8419f25
Ben Zhang <bzhang@thoughtbot.com>
2012-09-06 10:03:28 -0400

    update test to test omission option

--- spec/helpers/topics_helper_spec.rb
+++ spec/helpers/topics_helper_spec.rb
@@ -13,7 +13,7 @@ describe TopicsHelper, '#format_content' do
     content   = 'On the new learn homepage, the topic excerpts are too long. It would be additionally great to add a function into the view that controls the numbers of words present, so a designer can fool with it.'
     expected  = "On the new learn homepage, the topic excerpts are too long. It would be additionally great to add a function into the view that#{ellipsis}"
     helper.format_content(content).should == expected
-    helper.format_content(content, length: 10).should == "On&#8230;"
+    helper.format_content(content, length: 9, omission: "...").should == "On the..."
   end
 
   it 'does not include an ellipsis if the content is short' do
