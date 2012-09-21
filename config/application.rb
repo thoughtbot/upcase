eefaca31f077c16b830293935344610911938bce
Chad Pytel <cpytel@thoughtbot.com>
2012-09-21 17:03:29 -0400

    add airbrake to topics blacklist

--- config/application.rb
+++ config/application.rb
@@ -51,4 +51,4 @@ module Workshops
   end
 end
 
-BLACKLIST_TOPICS = ["this week in open source", "thoughtbot", "hoptoad"]
+BLACKLIST_TOPICS = ["this week in open source", "thoughtbot", "hoptoad", "airbrake"]
