282ba37a879ab64f13de2431c9a84d90164d4b19
Ben Whitla <benwhitla@Bens-MacBook-Pro.local>
2012-09-27 16:33:31 -0400

    Design progress

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,8 @@ Workshops::Application.routes.draw do
     resources :purchases, only: :index
   end
 
+  match 'pages/new-topics' => 'pages#show', id: 'new-topics'
+
   match '/auth/:provider/callback', to: 'auth_callbacks#create'
 
   match '/watch' => 'high_voltage/pages#show', as: :watch, id: 'watch'
