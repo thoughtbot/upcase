0898bdb9033ecb29cafcd5d01285a973be44fc6b
Caleb Thompson <cjaysson@gmail.com>
2012-10-09 13:06:35 -0400

    Move view link below articles, cleanup

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 
 describe ArticlesController do
-
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
       response.should be_success
     end
   end
-
 end
