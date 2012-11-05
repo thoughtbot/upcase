771ccc511ed31920fd904d2d552f266ae6d002a8
Chad Pytel <cpytel@thoughtbot.com>
2012-11-05 10:13:45 -0500

    fix for itemstyle invalid hash

--- app/controllers/reports/purchases_charts_controller.rb
+++ app/controllers/reports/purchases_charts_controller.rb
@@ -42,7 +42,7 @@ class Reports::PurchasesChartsController < ApplicationController
       colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
         '#FF9655', '#FFF263', '#6AF9C4'],
       legend: {
-        borderWidth: 0
+        borderWidth: 0,
         itemStyle: {
           color: '#666'
         }
