0bfa73134fb6cadfe82df090a72495702f93aad6
Chad Pytel <cpytel@thoughtbot.com>
2012-11-19 21:45:11 -0500

    Only show active products on index Fixes https://thoughtbot.apptrajectory.com/thoughtbot/learn/stories/117784

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -17,9 +17,11 @@ describe 'Products' do
     it 'lists all books' do
       book_one = create(:book_product)
       book_two = create(:book_product)
+      inactive_book = create(:book_product, active: false)
       visit products_path
       expect(page).to have_content(book_one.name)
       expect(page).to have_content(book_two.name)
+      expect(page).not_to have_content(inactive_book.name)
       click_link book_one.name
       expect(current_path).to eq(product_path(book_one))
     end
@@ -27,9 +29,11 @@ describe 'Products' do
     it 'lists all videos' do
       video_one = create(:video_product)
       video_two = create(:video_product)
+      inactive_video = create(:video_product, active: false)
       visit products_path
       expect(page).to have_content(video_one.name)
       expect(page).to have_content(video_two.name)
+      expect(page).not_to have_content(inactive_video.name)
       click_link video_one.name
       expect(current_path).to eq(product_path(video_one))
     end
@@ -37,9 +41,11 @@ describe 'Products' do
     it 'lists all workshops' do
       workshop_one = create(:workshop_product)
       workshop_two = create(:workshop_product)
+      inactive_workshop = create(:workshop_product, active: false)
       visit products_path
       expect(page).to have_content(workshop_one.name)
       expect(page).to have_content(workshop_two.name)
+      expect(page).not_to have_content(inactive_workshop.name)
       click_link workshop_one.name
       expect(current_path).to eq(product_path(workshop_one))
     end
