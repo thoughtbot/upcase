9d6a816b87e5a718309c6b41366d8332d25bd5bc
Chad Pytel <cpytel@thoughtbot.com>
2012-12-15 22:24:55 -0500

    also link to download counter from show page

--- features/step_definitions/episode_steps.rb
+++ features/step_definitions/episode_steps.rb
@@ -7,5 +7,5 @@ end
 
 Then /^I should see an audio player for the episode "([^"]*)"$/ do |episode_title|
   episode = Episode.find_by_title!(episode_title)
-  page.should have_css("audio source[src='#{episode.file}']")
+  page.should have_css("audio source[src='#{episode_path(episode, format: :mp3)}']")
 end
