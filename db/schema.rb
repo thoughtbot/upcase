eb7cc8352e80f0ab46c5987ec9301305b6c5da1e
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-01-02 11:34:07 -0500

    Use background jobs for GitHub API in Purchase

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121214194659) do
+ActiveRecord::Schema.define(:version => 20130102163245) do
 
   create_table "announcements", :force => true do |t|
     t.datetime "created_at",        :null => false
@@ -105,6 +105,22 @@ ActiveRecord::Schema.define(:version => 20121214194659) do
   end
 
   add_index "courses", ["audience_id"], :name => "index_courses_on_audience_id"
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "downloads", :force => true do |t|
     t.integer  "purchaseable_id"
