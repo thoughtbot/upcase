165aad73f0ef6ad88ed99bce1a2854df33231bff
Melissa Xie <mlxie8289@gmail.com>
2013-01-07 16:49:25 -0500

    Fix load order bug
    config/environments/*.rb files were being required before
    config/initializers/*.rb and so MAIL_SETTINGS constant was not available

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,10 +1,12 @@
 if Rails.env.staging? || Rails.env.production?
-  MAIL_SETTINGS = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
-  }
+  if ! defined?(MAIL_SETTINGS)
+    MAIL_SETTINGS = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com'
+    }
+  end
 end
