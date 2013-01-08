53f212eb9f9c91346f0e6cf255253cbcdb6e6902
Gabe Berke-Williams and Melissa Xie <support+gabe+melissa@thoughtbot.com>
2013-01-08 17:17:01 -0500

    Send purchase receipts in a background job

---
+++ app/jobs/send_purchase_receipt_email_job.rb
@@ -0,0 +1,10 @@
+class SendPurchaseReceiptEmailJob < Struct.new(:purchase_id)
+  def self.enqueue(purchase_id)
+    Delayed::Job.enqueue(new(purchase_id))
+  end
+
+  def perform
+    purchase = Purchase.find(purchase_id)
+    Mailer.purchase_receipt(purchase).deliver
+  end
+end
