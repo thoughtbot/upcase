9205e3cbda4fd7b0567dcfb86d7e7a08b04799a9
Melissa Xie <mlxie8289@gmail.com>
2013-01-09 17:01:49 -0500

    Move EMAIL_FORMAT constant to the top

--- app/models/follow_up.rb
+++ app/models/follow_up.rb
@@ -1,6 +1,7 @@
 class FollowUp < ActiveRecord::Base
-  belongs_to :course
   EMAIL_FORMAT = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  belongs_to :course
   validates :email, presence: true, format: { with: EMAIL_FORMAT }, on: :create
 
   scope :have_not_notified, where(notified_at: nil)
