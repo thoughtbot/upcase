9f7a66cf0c9a29c016e883d9ab8dc61754816ea6
Chad Pytel <cpytel@thoughtbot.com>
2013-01-17 23:05:15 -0500

    try to fix rfc822 on production

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -3,7 +3,8 @@
 #
 {
   short_date: '%x',
-  simple: '%B %d, %Y'
+  simple: '%B %d, %Y',
+  rfc822: '%a, %d %b %Y %H:%M:%S %z'
 }.each do |k, v|
   Date::DATE_FORMATS[k] = v
   Time::DATE_FORMATS[k] = v
@@ -14,5 +15,5 @@ end
 #
 Time::DATE_FORMATS.merge!({
   month: '%B',
-  time: '%l:%M%p'
+  time: '%l:%M%p',
 })
