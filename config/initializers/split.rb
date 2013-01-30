23d19035a915de3f47d13f9bf0df9a96b6c5363d
Chad Pytel <cpytel@thoughtbot.com>
2013-01-29 22:42:57 -0500

    configure split, allow multipl eexpirements

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -1,2 +1,10 @@
+Split.configure do |config|
+  config.allow_multiple_experiments = true
+end
+
+Split::Dashboard.use Rack::Auth::Basic do |username, password|
+  username == 'admin' && password == 'J7t9VDGZZxPTb'
+end
+
 Split.redis = ENV['REDISTOGO_URL'] || 'redis://localhost:6379'
 Split.redis.namespace = "split:workshops"
