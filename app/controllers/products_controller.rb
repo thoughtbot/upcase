e29c2408421405b33022b901ebf144222744b3b1
Chad Pytel <cpytel@thoughtbot.com>
2013-02-25 14:06:21 -0500

    begin implementation of header dropdowns

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,5 @@
 class ProductsController < ApplicationController
   def index
-    @in_person_workshops = Workshop.only_public.by_position.in_person
-    online_workshops = Workshop.only_public.by_position.online
-    # This `workshops = `line can be removed on 2013-01-16, when
-    # Product.workshops will be empty and all online workshops will be in
-    # Workshop.
-    workshop_products = Product.workshops.active.ordered
-    @online_workshops = online_workshops + workshop_products
-    @books = Product.books.active.ordered
-    @videos = Product.videos.active.ordered
-    @articles = Article.local.top.published
   end
 
   def show
