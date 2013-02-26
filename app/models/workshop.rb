68fc72bc3d6e7e6c09e973fd8c7112153931babb
Ben Orenstein <ben.orenstein@gmail.com>
2013-02-26 12:51:44 -0500

    Remove A/B testing code for workshop/product prices

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -35,14 +35,6 @@ class Workshop < ActiveRecord::Base
     sections.active
   end
 
-  def alternate_company_price
-    self[:alternate_company_price] || company_price
-  end
-
-  def alternate_individual_price
-    self[:alternate_individual_price] || individual_price
-  end
-
   def announcement
     @announcement ||= announcements.current
   end
