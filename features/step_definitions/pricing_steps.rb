4707eed0ae22e7a25b938585fbcbe098a4d8b42c
Ben Orenstein <ben.orenstein@gmail.com>
2013-02-26 14:37:24 -0500

    Pull step

--- features/step_definitions/pricing_steps.rb
+++ features/step_definitions/pricing_steps.rb
@@ -15,16 +15,3 @@ end
 Then /^I should see that the company workshop price is "([^"]+)"$/ do |price|
   find(".company-purchase span").should have_content price
 end
-
-Then /^the product purchase variants should be (primary|alternate)$/ do |purchase_variant|
-  individual_variant = find('.individual-purchase .license a')[:href]
-  company_variant = find('.company-purchase .license a')[:href]
-
-  if purchase_variant == 'primary'
-    individual_variant.should include '?variant=individual'
-    company_variant.should include '?variant=company'
-  else
-    individual_variant.should include '?variant=alternate_individual'
-    company_variant.should include '?variant=alternate_company'
-  end
-end
