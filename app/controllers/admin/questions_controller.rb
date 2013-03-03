a49293ef2f930eefa853b92abebc4afa87fc7d6b
Chad Pytel <cpytel@thoughtbot.com>
2013-03-03 19:26:07 +0100

    remove custom admin and replace with rails_admin

--- app/controllers/admin/questions_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::QuestionsController < AdminController
-  def destroy
-    workshop = Workshop.find(params[:workshop_id])
-    question = workshop.questions.find(params[:id])
-    question.destroy
-    flash[:notice] = "Question removed"
-    redirect_to edit_admin_workshop_path(workshop)
-  end
-end
