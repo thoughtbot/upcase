0bd88439cbe9d3f234902d0a38ed38ee4f155455
Chad Pytel <cpytel@thoughtbot.com>
2013-03-03 19:26:07 +0100

    remove custom admin and replace with rails_admin

--- app/controllers/admin/positions_controller.rb
+++
@@ -1,7 +0,0 @@
-class Admin::PositionsController < AdminController
-  def update
-    @workshop = Workshop.find(params[:workshop_id])
-    @workshop.insert_at(params[:position].to_i)
-    render nothing: true
-  end
-end
