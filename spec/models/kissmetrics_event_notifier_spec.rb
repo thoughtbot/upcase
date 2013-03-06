946deffb815725455221ed47b9ce373d6a0ddb60
Ben Orenstein <ben.orenstein@gmail.com>
2013-03-06 15:38:03 -0500

    Track monthly rebilling for Prime via Stripe webhook
    * Adds stripe_event gem to handle Stripe's webhook
    
    * Sends a new event to Kissmetrics: 'Subscription Billed', which we send
    when we bill for Prime
    
    * Adds a new endpoint to FakeStripe. This is because stripe_event gets an
    id of an event from Stripe, and then requests the full event from them
    (as a security measure).

--- spec/models/kissmetrics_event_notifier_spec.rb
+++ spec/models/kissmetrics_event_notifier_spec.rb
@@ -1,43 +1,97 @@
 require 'spec_helper'
 
 describe KissmetricsEventNotifier do
-  context 'when the purchase is paid' do
-    it 'notifies Kissmetrics of a Billed event' do
-      purchase = paid_non_subscription_purchase
-      notifier.notify_of(purchase)
+  context '#notify_of_purchase' do
+    context 'when the purchase is paid' do
+      context 'when the purchase is a subscription' do
+        it 'notifies Kissmetrics of a Signed Up event' do
+          purchase = paid_subscription_purchase
 
-      client.should have_received(:record).with(purchase.email,
-        'Billed',
-        { 'Product Name' => purchase.purchaseable_name, 'Amount Billed' => purchase.price })
-    end
+          notifier.notify_of_purchase(purchase)
+          client.should have_received(:record).with(purchase.email,
+            'Signed Up',
+            { 'Plan Name' => purchase.purchaseable_sku })
+        end
 
-    context 'when the purchase is a subscription' do
-      it 'notifies Kissmetrics of a Signed Up event' do
-        purchase = paid_subscription_purchase
+        it 'does not notify Kissmetrics of a Subscription Billed event' do
+          purchase = paid_subscription_purchase
 
-        notifier.notify_of(purchase)
-        client.should have_received(:record).with(purchase.email,
-          'Signed Up',
-          { 'Plan Name' => purchase.purchaseable_sku })
+          notifier.notify_of_purchase(purchase)
+
+          client.should have_received(:record).with(
+            purchase.email,
+            'Subscription Billed',
+            { 'Subscription Billing Amount' => purchase.price }).never
+        end
+      end
+
+      context 'when the purchase is not a subscription' do
+        it 'notifies Kissmetrics of a Billed event' do
+          purchase = paid_non_subscription_purchase
+
+          notifier.notify_of_purchase(purchase)
+
+          client.should have_received(:record).with(purchase.email,
+            'Billed',
+            { 'Product Name' => purchase.purchaseable_name, 'Amount Billed' => purchase.price })
+        end
+
+        it 'does not notify Kissmetrics of a Signed Up event' do
+          purchase = paid_non_subscription_purchase
+
+          notifier.notify_of_purchase(purchase)
+
+          client.should have_received(:record).with(purchase.email,
+            'Signed Up',
+            { 'Plan Name' => purchase.purchaseable_sku }).never
+        end
       end
     end
 
-    context 'when the purchase is not a subscription' do
-      it 'does not notify Kissmetrics of a Signed Up event' do
-        purchase = paid_non_subscription_purchase
-
-        notifier.notify_of(purchase)
-        client.should have_received(:record).with(purchase.email,
-          'Signed Up',
-          { 'Plan Name' => purchase.purchaseable_sku }).never
+    context 'when the purchase is not paid' do
+      it 'does not notify Kissmetrics of a Billed event' do
+        notifier.notify_of_purchase(unpaid_purchase)
+        client.should have_received(:record).never
       end
+    end
+
+    def paid_non_subscription_purchase
+      paid_purchase.tap do |stub|
+        stub.stubs(subscription?: false, purchaseable_sku: 'book')
+      end
+    end
+
+    def paid_subscription_purchase
+      paid_purchase.tap do |stub|
+        stub.stubs(subscription?: true, purchaseable_sku: 'prime')
+      end
+    end
+
+    def paid_purchase
+      purchase.tap do |stub|
+        stub.stubs(paid?: true)
+      end
+    end
+
+    def unpaid_purchase
+      purchase.tap do |stub|
+        stub.stubs(paid?: false)
+      end
+    end
+
+    def purchase
+      stub('purchase', email: 'foo@bar.com', purchaseable_name: 'Product Name', price: 99, id: 123)
     end
   end
 
-  context 'when the purchase is not paid' do
-    it 'does not notify Kissmetrics of a Billed event' do
-      notifier.notify_of(unpaid_purchase)
-      client.should have_received(:record).never
+  context '#notify_of_subscription_billing' do
+    it 'notifies Kissmetrics of a Subscription Billed event' do
+      notifier.notify_of_subscription_billing('foo@bar.com', 99)
+
+      client.should have_received(:record).with(
+        'foo@bar.com',
+        'Subscription Billed',
+        { 'Subscription Billing Amount' => 99 })
     end
   end
 
@@ -47,33 +101,5 @@ describe KissmetricsEventNotifier do
 
   def notifier
     @notifier ||= KissmetricsEventNotifier.new(client)
-  end
-
-  def paid_non_subscription_purchase
-    paid_purchase.tap do |stub|
-      stub.stubs(subscription?: false, purchaseable_sku: 'book')
-    end
-  end
-
-  def paid_subscription_purchase
-    paid_purchase.tap do |stub|
-      stub.stubs(subscription?: true, purchaseable_sku: 'prime')
-    end
-  end
-
-  def paid_purchase
-    purchase.tap do |stub|
-      stub.stubs(paid?: true)
-    end
-  end
-
-  def unpaid_purchase
-    purchase.tap do |stub|
-      stub.stubs(paid?: false)
-    end
-  end
-
-  def purchase
-    stub('purchase', email: 'foo@bar.com', purchaseable_name: 'Product Name', price: 99)
   end
 end
