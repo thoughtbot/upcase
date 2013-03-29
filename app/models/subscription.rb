3ae364b9f0bbca5953bf5d1c7205049ed69b189c
Joel Quenneville <joel.quen@gmail.com>
2013-03-29 14:02:35 -0400

    extract the recent scope on Subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,8 +3,12 @@ class Subscription < ActiveRecord::Base
   belongs_to :user
   delegate :stripe_customer, to: :user
 
+  def self.recent
+    where('created_at > ?', 24.hours.ago)
+  end
+
   def self.send_welcome_emails
-    where('created_at > ?', 24.hours.ago).each do |subscription|
+    recent.each do |subscription|
       Mailer.welcome_to_prime(subscription.user).deliver
     end
   end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,6 +3,15 @@ require 'spec_helper'
 describe Subscription do
   it { should delegate(:stripe_customer).to(:user) }
 
+  describe '.recent' do
+    it 'returns the subscriptions that are less than 24 hours old' do
+      old_subscription = create :subscription, created_at: 25.hours.ago
+      new_subscription = create :subscription, created_at: 10.hours.ago
+
+      expect(Subscription.recent).to eq([new_subscription])
+    end
+  end
+
   describe '.send_welcome_emails' do
     it 'sends emails for each new subscriber in the last 24 hours' do
       old_subscription = create :subscription, created_at: 25.hours.ago
