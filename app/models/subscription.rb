618bebf5dffed8652e1a49f39a6b752b13ac87d6
Joel Quenneville <joel.quen@gmail.com>
2013-03-29 13:34:12 -0400

    send welcome emails to everyone who subscribed to prime yesterday

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,10 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
   delegate :stripe_customer, to: :user
+
+  def self.send_welcome_emails
+    where('created_at > ?', 24.hours.ago).each do |subscription|
+      Mailer.welcome_to_prime(subscription.user).deliver
+    end
+  end
 end
