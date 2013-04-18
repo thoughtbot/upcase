9bba62927deb8a4c720c8ffcabe7eb96cc4beb72
George Brocklehurst <george.brocklehurst@gmail.com>
2013-04-18 16:10:50 +0200

    Ask users why they unsubscribed from Prime.

--- app/models/unsubscriber.rb
+++ app/models/unsubscriber.rb
@@ -8,6 +8,11 @@ class Unsubscriber
       @subscription.destroy
       stripe_user = Stripe::Customer.retrieve(@subscription.stripe_customer)
       stripe_user.cancel_subscription
+      deliver_unsubscription_survey
     end
+  end
+
+  def deliver_unsubscription_survey
+    Mailer.unsubscription_survey(@subscription.user).deliver
   end
 end
