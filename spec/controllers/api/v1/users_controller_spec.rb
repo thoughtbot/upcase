3a0af166a4bed26fcaf741a91cadac5bda179314
Ben Orenstein <ben.orenstein@gmail.com>
2013-04-18 10:29:38 -0400

    Small refactorings
    * Disable logging to silence noise during test
    * Use shortcut for defining and booting Discoball with one method
    * Prefer eq over ==

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Api::V1::UsersController, '#show' do
   it 'returns a 401 when users are not authenticated' do
     get :show
-    response.code.should == '401'
+    response.code.should eq '401'
   end
 end
