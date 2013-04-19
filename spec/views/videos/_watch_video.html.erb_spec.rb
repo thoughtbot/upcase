193cfd1ff075a33d535ab5722849a259c7de51de
patrick brisbin <pbrisbin@gmail.com>
2013-04-19 15:25:46 -0400

    include notes in video view

---
+++ spec/views/videos/_watch_video.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'videos/_watch_video.html.erb' do
+  it "includes a video's notes" do
+    video = build_stubbed(:video, notes: 'Some notes')
+
+    render_view(video)
+
+    rendered.should include(video.notes)
+  end
+
+  it "can still render a video without notes" do
+    video = build_stubbed(:video)
+
+    expect { render_view(video) }.to_not raise_error
+  end
+
+  def render_view(video)
+    purchaseable = build_stubbed(:video_product)
+
+    render 'videos/watch_video', video: video, purchaseable: purchaseable
+  end
+end
