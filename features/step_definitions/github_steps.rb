471813a51e49bd48917469b74a3196d5bb86168f
Chad Pytel <cpytel@thoughtbot.com>
2013-04-26 14:17:55 -0400

    Revert "fix webmock for octokit change"
    This reverts commit eb53876d85ca316df9a6f1f2ce214a71b9757292.

--- features/step_definitions/github_steps.rb
+++ features/step_definitions/github_steps.rb
@@ -1,7 +1,7 @@
 Given /^github is stubbed$/ do
   auth = [GITHUB_USER, GITHUB_PASSWORD].join(':')
   auth = "#{auth}@" unless auth.blank?
-  stub_request(:put, "https://#{auth}api.github.com/teams/members/cpytel").with(:body => '{"name":"cpytel"}', :headers => {'Accept'=>'application/vnd.github.beta+json', 'Content-Type'=>'application/json'}).to_return(:status => 200, :body => "", :headers => {})
+  stub_request(:put, "https://#{auth}api.github.com/teams/members/cpytel").with(:body => '{"name":"cpytel"}', :headers => {'Accept'=>'*/*', 'Content-Type'=>'application/json', 'User-Agent'=>'Ruby'}).to_return(:status => 200, :body => "", :headers => {})
 end
 
 Then /^I should see a github username error$/ do
