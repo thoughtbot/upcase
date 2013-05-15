31fc877b1aa5f926237148cbc7def0d7458e980b
Chad Pytel <cpytel@thoughtbot.com>
2013-05-15 12:26:51 -0400

    fix for method name change

--- app/controllers/reports/purchases_charts_controller.rb
+++ app/controllers/reports/purchases_charts_controller.rb
@@ -28,7 +28,8 @@ class Reports::PurchasesChartsController < ApplicationController
     last_twelve_months.collect do |index|
       startdate = index.months.ago.beginning_of_month
       enddate = index.months.ago.end_of_month
-      Purchase.for_purchaseable(product).within_range(startdate, enddate).to_i
+      Purchase.for_purchaseable(product).
+        total_sales_within_range(startdate, enddate).to_i
     end
   end
 
