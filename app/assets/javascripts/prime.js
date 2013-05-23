c39863b9ffc4076e98adc2a9ad74ae2cb9b839d6
Edwin <edwin.h.morris@gmail.com>
2013-05-23 12:02:36 -0400

    Add terms to prime marketing page, remove bottom section * integrate prime bytes into screencasts/books section * add footer w/ policy, blog, etc. links * reorganize css * extract "buffet" from marketing page for later use * remove animations on screens less than 800px wide to fix iOS crashes * refine copy * https://www.apptrajectory.com/thoughtbot/learn/stories/15628827 * https://www.apptrajectory.com/thoughtbot/learn/stories/15628825

--- app/assets/javascripts/prime.js
+++ app/assets/javascripts/prime.js
@@ -9,46 +9,33 @@ if ($('.journey').length) {
     jsPlumbSettings.detachEveryConnection();
     jsPlumbSettings.removeAllEndpoints();
 
+
     jsPlumbSettings.connect({
-      connector:['Bezier', { curviness: 120 }],
-      source: 'first-cta',
-      target: 'bullet-1',
-      anchors:['Bottom', 'Top'],
-      endpoint: ['Dot', { cssClass: 'connector-dot' }]
-    });
-    jsPlumbSettings.connect({
-      connector:['Bezier', { curviness: 300 }],
+      connector:['Bezier', { curviness: 340 }],
       source: 'bullet-1',
       target: 'bullet-2',
       anchors:['Bottom', 'Top'],
       endpoint: ['Dot', { cssClass: 'connector-dot' }]
     });
     jsPlumbSettings.connect({
-      connector:['Bezier', { curviness: 420 }],
+      connector:['Bezier', { curviness: 340 }],
       source: 'bullet-2',
       target: 'bullet-3',
       anchors:['Bottom', 'Top'],
       endpoint: ['Dot', { cssClass: 'connector-dot' }]
     });
     jsPlumbSettings.connect({
-      connector:['Bezier', { curviness: 300 }],
+      connector:['Bezier', { curviness: 340 }],
       source: 'bullet-3',
       target: 'bullet-4',
       anchors:['Bottom', 'Top'],
       endpoint: ['Dot', { cssClass: 'connector-dot' }]
     });
     jsPlumbSettings.connect({
-      connector:['Bezier', { curviness: 420 }],
+      connector:['Bezier', { curviness: 340 }],
       source: 'bullet-4',
       target: 'bullet-5',
       anchors:['Bottom', 'Top'],
-      endpoint: ['Dot', { cssClass: 'connector-dot' }]
-    });
-    jsPlumbSettings.connect({
-      connector:['Bezier', { curviness: 300 }],
-      source: 'bullet-5',
-      target: 'detail-figure',
-      anchors:['Bottom', 'TopLeft'],
       endpoint: ['Dot', { cssClass: 'connector-dot' }]
     });
   };
