ddb1ac939e9d6b52e9edb3e079daa06d89db4248
Edwin <edwin.h.morris@gmail.com>
2013-05-31 16:32:22 -0400

    color and style bytes and books sections * create mixin function for gridding by product card width * refactor slider js, enable/disable slider nav * https://www.apptrajectory.com/thoughtbot/learn/stories/15629291

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,29 +1,55 @@
 $(function() {
   if ($('.card-slider').length) {
-    // add for to account for inline-block 4px space character
+    // +4 to account for inline-block 4px space character
     var card_width = $('.product-card:first').outerWidth(true) + 4;
 
-    function slider_not_at_beginning(slider) {
-      return parseInt(slider.find('.product-card:first').css('margin-left'), 10) < 0;
+    function slider_at_beginning(slider) {
+      return parseInt(slider.find('.product-card:first').css('margin-left'), 10) >= 0;
     }
 
-    function slider_not_at_end(slider) {
-      return (slider.find('.product-card:last').position().left / slider.width()) > 1;
+    function slider_at_end(slider) {
+      return (slider.find('.product-card:last').position().left / slider.width()) <= 1;
     }
 
-    $('.card-slider .nav').on('click', function() {
-      if ($(this).hasClass('next') &&
-          slider_not_at_end($(this).parent('.card-slider'))) {
-        $(this).siblings('.product-card:first').css({
+    function toggle_nav_items(slider) {
+      slider.children('.nav').removeClass('disabled');
+
+      if (slider_at_beginning(slider)) {
+        slider.children('.prev').addClass('disabled');
+      }
+      if (slider_at_end(slider)) {
+        slider.children('.next').addClass('disabled');
+      }
+    }
+
+    function slide_one_card(slider, direction) {
+      if (direction == 'left') {
+        slider.children('.product-card:first').css({
           'margin-left': "-="+card_width+"px"
         });
       }
-      else if ($(this).hasClass('prev') && 
-               slider_not_at_beginning($(this).parent('.card-slider'))) {
-        $(this).siblings('.product-card:first').css({
+      if (direction == 'right') {
+        slider.children('.product-card:first').css({
           'margin-left': "+="+card_width+"px"
         });
       }
+    }
+
+    $('.card-slider').each(function() {
+      toggle_nav_items($(this));
+    });
+
+    $('.card-slider .nav').on('click', function() {
+      var slider = $(this).parent('.card-slider');
+
+      if ($(this).hasClass('next') && !slider_at_end(slider)) {
+        slide_one_card(slider, 'left');
+      }
+      if ($(this).hasClass('prev') && !slider_at_beginning(slider)) {
+        slide_one_card(slider, 'right');
+      }
+
+      toggle_nav_items(slider);
     });
   }
 });
