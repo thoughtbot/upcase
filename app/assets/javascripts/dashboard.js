03d69bdc427cc3111145d51711748a6767f1c923
Edwin <edwin.h.morris@gmail.com>
2013-06-04 16:21:45 -0400

    improve slider visual design, refactor js * refine slider arrow * use text dates for in-person workshops * prevent the slider from ending in the middle of a card * iterate by whole card widths instead of incrementing a current value

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -22,34 +22,38 @@ $(function() {
       }
     }
 
-    function slide_one_card(slider, direction) {
-      if (direction == 'left') {
+    // slide the slider by moving the first card around via margin if the
+    // card is in mid-transition, wait until it finishes to add the margin,
+    // so that the margin remains divisible by one card width
+    function slide_one_card(slider) {
+      if (!slider_at_end(slider) || !slider_at_beginning(slider)) {
         slider.children('.product-card:first').css({
-          'margin-left': "-="+card_width+"px"
-        });
-      }
-      if (direction == 'right') {
-        slider.children('.product-card:first').css({
-          'margin-left': "+="+card_width+"px"
+          'margin-left': parseInt(- card_width * slider.data('index'), 10)+'px'
         });
       }
     }
 
     $('.card-slider').each(function() {
       toggle_nav_items($(this));
+      $(this).data('index', 0);
     });
 
     $('.card-slider .nav').on('click', function() {
       var slider = $(this).parent('.card-slider');
 
       if ($(this).hasClass('next') && !slider_at_end(slider)) {
-        slide_one_card(slider, 'left');
+        slider.data('index', slider.data('index') + 1);
       }
       if ($(this).hasClass('prev') && !slider_at_beginning(slider)) {
-        slide_one_card(slider, 'right');
+        slider.data('index', slider.data('index') - 1);
       }
 
-      toggle_nav_items(slider);
+      slide_one_card(slider);
+
+      // trigger navigation check after transition end
+      slider.children('.product-card:first').one('webkitTransitionEnd msTransitionEnd transitionend', function() {
+        toggle_nav_items(slider);
+      });
     });
   }
 });
