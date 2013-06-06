bb6073212f4704b895b05789b54be3ee7ec4963d
Ben Orenstein <ben.orenstein@gmail.com>
2013-06-06 14:38:29 -0400

    Extract another partial. Removes duplication in views and specs

---
+++ spec/views/products/_forum.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'products/_forum_link.html.erb' do
+  context 'when the user has a subscription' do
+    it 'includes a link to the forum' do
+      render partial: 'products/forum_link.html.erb', locals: { current_user: user_with_subscription }
+      expect(rendered).to include('View Forum')
+    end
+  end
+
+  context 'when the user does not have a subscription' do
+    it 'does not include a link to the forum' do
+      render partial: 'products/forum_link.html.erb', locals: { current_user: user_without_subscription }
+      expect(rendered).to_not include('View Forum')
+    end
+  end
+
+  def user_without_subscription
+    stub('user', has_active_subscription?: false)
+  end
+
+  def user_with_subscription
+    stub('user', has_active_subscription?: true)
+  end
+end
