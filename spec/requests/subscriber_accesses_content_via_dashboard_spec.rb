df82a81c50b943d189a25e0c53669c864ff03648
Ben Orenstein <ben.orenstein@gmail.com>
2013-06-06 10:38:29 -0400

    Test info link

--- spec/requests/subscriber_accesses_content_via_dashboard_spec.rb
+++ spec/requests/subscriber_accesses_content_via_dashboard_spec.rb
@@ -1,26 +1,14 @@
 require 'spec_helper'
 
 feature 'Subscriber accesses content' do
-  scenario 'views listing of all products' do
-    create_all_product_types
+  scenario 'accesses a workshop product detail page' do
+    workshop = create(:online_workshop)
     sign_in_as_user_with_subscription
-    save_and_open_page
+    within('.workshop-online') do
+      click_link 'Info'
+    end
 
-    expect(page).to have_content I18n.t('products.index.subscriber.books')
-    expect(page).to have_content I18n.t('products.index.videos')
-    expect(page).to have_content I18n.t('products.index.subscriber.bytes')
-
-    expect(page).to_not have_content I18n.t('shared.subscription_call_to_action')
-  end
-
-  scenario 'accesses a product detail page' do
-    video_product = create(:video_product)
-    sign_in_as_user_with_subscription
-    click_link video_product.name
-
-    expect(page).to have_content I18n.t('products.show.free_to_subscribers')
-    expect(page).to_not have_content "$#{video_product.individual_price}"
-    expect(page).to_not have_content I18n.t('products.show.purchase_for_company')
+    expect(current_path).to eq workshop_path(workshop)
   end
 
   scenario 'gets access to a book product' do
@@ -105,7 +93,8 @@ feature 'Subscriber accesses content' do
   def create_all_product_types
     create(:video_product)
     create(:book_product)
-    create(:workshop_product)
+    create(:online_workshop)
+    create(:in_person_workshop)
     create(:subscribeable_product)
   end
 
