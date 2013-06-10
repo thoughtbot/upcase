81eeb83aff43b08474dc9b94e6a5e169677a143f
Chad Pytel <cpytel@thoughtbot.com>
2013-06-10 15:51:12 -0400

    send to readmill button
    use small readmill button to get our own wording
    
    only disable readmill js in test
    
    style readmill button
    
    I am a designer!
    
    finalize readmill specs

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe 'A Purchased book' do
   context 'GET /purchases/show for a book' do
+    include ProductsHelper
+
     it 'does not include support' do
       book = create(:book_product, name: 'Book title')
       purchase = create(:paid_purchase, purchaseable: book)
@@ -19,6 +21,17 @@ describe 'A Purchased book' do
       visit purchase_path(purchase)
 
       expect(page).to have_content 'includes support'
+    end
+
+    it 'includes a send to readmill button' do
+      book = create(:github_book_product, name: 'Book title')
+      purchase = create(:paid_purchase, purchaseable: book)
+
+      visit purchase_path(purchase)
+
+      expect(page).to have_css(
+        "a.send-to-readmill[data-download-url='#{u epub_url(book)}'][data-buy-url='#{u product_url(book)}']"
+      )
     end
   end
 end
