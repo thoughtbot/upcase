3f49585fbac598380f1b9b2d233add1912b4b065
Mason Fischer <mason@thoughtbot.com>
2013-06-14 10:24:08 -0700

    Pass flash[:purchase_paid_price] as a parameter

--- spec/helpers/adwords_helper_spec.rb
+++ spec/helpers/adwords_helper_spec.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 
 describe AdwordsHelper, '#adwords_conversion_tracker' do
   it 'includes the conversion tracking javascript' do
-    flash[:purchase_paid_price] = 10
-
-    helper.adwords_conversion_tracker.should include 'conversion.js'
-  end
-
-  it 'returns nil if flash[:purchase_paid_price] is not set' do
-    helper.adwords_conversion_tracker.should eq nil
+    helper.adwords_conversion_tracker(10).should include 'conversion.js'
   end
 end
