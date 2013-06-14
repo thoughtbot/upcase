dbdec5b139b907dde01b301294775a23ec8d2947
Mason Fischer <mason@thoughtbot.com>
2013-06-14 10:41:16 -0700

    Move logic into helper

--- spec/helpers/adwords_helper_spec.rb
+++ spec/helpers/adwords_helper_spec.rb
@@ -4,4 +4,8 @@ describe AdwordsHelper, '#adwords_conversion_tracker' do
   it 'includes the conversion tracking javascript' do
     helper.adwords_conversion_tracker(10).should include 'conversion.js'
   end
+
+  it 'returns nil if flash[:purchase_paid_price] is not set' do
+    helper.adwords_conversion_tracker(nil).should eq nil
+  end
 end
