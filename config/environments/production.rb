32ac2d7774b6101f362b29b8c974850ee03b02b0
Chad Pytel <cpytel@thoughtbot.com>
2013-06-18 14:14:51 -0400

    use https for s3 in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,7 @@ Workshops::Application.configure do
   PAPERCLIP_STORAGE_OPTIONS = {
      storage: :s3,
      s3_credentials: "#{Rails.root}/config/s3.yml",
+     s3_protocol: 'https'
   }
 
   GITHUB_KEY = ENV['GITHUB_KEY']
