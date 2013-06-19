83ea574535504c00b55f0f4558ea69647cb4dfb1
Edwin <edwin.h.morris@gmail.com>
2013-06-19 16:27:11 -0400

    show active state on a workshop if it has been purchased
    refactor for dryness
    
    Conflicts:
    	app/models/section.rb
    	app/views/workshops/_terms.html.erb
    
    add coverage on active class on dashboard workshop
    
    use online and in_person workshop factories
    
    move product-card class into workshop_card_classes

--- spec/helpers/workshops_helper_spec.rb
+++ spec/helpers/workshops_helper_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe WorkshopsHelper, '#workshop_frequency_note' do
   context 'an workshop that starts immediately' do
     it 'says how often it is offered' do
-      workshop = stub(online?: true, starts_immediately?: true)
+      workshop = stub(fulfillment_method: 'online', starts_immediately?: true)
 
       note = workshop_frequency_note(workshop)
 
@@ -13,7 +13,7 @@ describe WorkshopsHelper, '#workshop_frequency_note' do
 
   context 'a workshop that does not start immediately' do
     it 'says how often it is offered' do
-      workshop = stub(online?: true, starts_immediately?: false)
+      workshop = stub(fulfillment_method: 'online', starts_immediately?: false)
 
       note = workshop_frequency_note(workshop)
 
