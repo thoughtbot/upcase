48a12daf40b5696b819fb38e2b8331fb811bfc83
Chad Pytel <cpytel@thoughtbot.com>
2013-06-24 12:14:41 -0400

    double opt in subscribers

--- app/jobs/mailchimp_fulfillment_job.rb
+++ app/jobs/mailchimp_fulfillment_job.rb
@@ -1,9 +1,15 @@
 class MailchimpFulfillmentJob < MailchimpJob
-  MASTER_LIST_ID = '719485'
+  MASTER_LIST_ID = '66f4d45e54'
 
   def perform
     lists = client.lists(filters: { list_name: list_name })
-    client.list_subscribe(id: lists['data'].first['id'], email_address: email)
-    client.list_subscribe(id: MASTER_LIST_ID, email_address: email)
+    subscribe(lists['data'].first['id'], email)
+    subscribe(MASTER_LIST_ID, email)
+  end
+
+  private
+
+  def subscribe(list_id, email)
+    client.list_subscribe(id: list_id, email_address: email, double_optin: true)
   end
 end
