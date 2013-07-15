024cc139f357245301876ffdd147a4d7d47f73c6
Chad Pytel <cpytel@thoughtbot.com>
2013-07-15 11:52:10 -0400

    display a date range for online workshops too https://www.apptrajectory.com/thoughtbot/learn/stories/15630458
    This involved extracting the date range helper in order to ensure that
    the logic for purchase dates was in a place that made sense and DRY
    
    extract to partials for clarity

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -56,4 +56,22 @@ module PurchasesHelper
   def subscription_interval(purchase)
     purchase.purchaseable.subscription_interval
   end
+
+  def purchase_date_range(purchase)
+    formatted_date_range(purchase.starts_on, purchase.ends_on)
+  end
+
+  def formatted_date_range(starts_on, ends_on)
+    if starts_on.nil? || ends_on.nil?
+      nil
+    elsif starts_on == ends_on
+      starts_on.to_s :simple
+    elsif starts_on.year != ends_on.year
+      "#{starts_on.to_s(:simple)}-#{ends_on.to_s(:simple)}"
+    elsif starts_on.month != ends_on.month
+      "#{starts_on.strftime('%B %d')}-#{ends_on.to_s(:simple)}"
+    else
+      "#{starts_on.strftime('%B %d')}-#{ends_on.strftime('%d, %Y')}"
+    end
+  end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -98,20 +98,6 @@ class Section < ActiveRecord::Base
     'workshop'
   end
 
-  def date_range
-    if starts_on.nil? || ends_on.nil?
-      nil
-    elsif starts_on == ends_on
-      starts_on.to_s :simple
-    elsif starts_on.year != ends_on.year
-      "#{starts_on.to_s(:simple)}-#{ends_on.to_s(:simple)}"
-    elsif starts_on.month != ends_on.month
-      "#{starts_on.strftime('%B %d')}-#{ends_on.to_s(:simple)}"
-    else
-      "#{starts_on.strftime('%B %d')}-#{ends_on.strftime('%d, %Y')}"
-    end
-  end
-
   def full?
     if seats_available.present?
       purchases.count >= seats_available
