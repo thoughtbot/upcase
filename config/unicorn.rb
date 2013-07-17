7def7dfbf0c484ae9f67da4611dbc6ed29a737d9
Arun Agrawal <arunagw@gmail.com>
2013-07-17 15:55:40 +0200

    Using unicorn to app
    I have seen that using unicorn will give us
    performance and extra web workers on heroku
    
    Right now we have 4 dynos which are 1 process
    per dyno because we are using thin
    
    Using unicorn will give us 2 worker each dyno
    So we are going to have 8 web workers in 4 dynos

---
+++ config/unicorn.rb
@@ -0,0 +1,20 @@
+worker_processes [ENV['UNICORNS'].to_i, 2].max
+timeout 25
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+end
