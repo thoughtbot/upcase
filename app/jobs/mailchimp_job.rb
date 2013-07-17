ff8f839d9f9ca5d76764615e1cf993a3a835e472
Chad Pytel <cpytel@thoughtbot.com>
2013-07-17 13:34:25 -0400

    rescue certain mailchimp errors https://www.apptrajectory.com/thoughtbot/learn/stories/15631866
    fix typo in docstring
    
    moved FakeMailchimp error handling into method

--- app/jobs/mailchimp_job.rb
+++ app/jobs/mailchimp_job.rb
@@ -2,6 +2,7 @@ class MailchimpJob < Struct.new(:list_name, :email)
   include ErrorReporting
 
   PRIORITY = 1
+  MAILCHIMP_EMAIL_ERROR_CODES = [230, 231, 232, 233]
 
   def self.enqueue(list_name, email)
     Delayed::Job.enqueue(new(list_name, email))
