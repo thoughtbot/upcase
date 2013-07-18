1c843c9a9cb97870c57fa750ae7cf56a5eb9a9a0
Chad Pytel <cpytel@thoughtbot.com>
2013-07-18 16:28:24 -0400

    code 214, code 214

--- app/jobs/mailchimp_job.rb
+++ app/jobs/mailchimp_job.rb
@@ -2,7 +2,8 @@ class MailchimpJob < Struct.new(:list_name, :email)
   include ErrorReporting
 
   PRIORITY = 1
-  MAILCHIMP_EMAIL_ERROR_CODES = [230, 231, 232, 233]
+  # http://apidocs.mailchimp.com/api/1.3/exceptions.field.php
+  MAILCHIMP_EMAIL_ERROR_CODES = [230, 231, 232, 233, 214]
 
   def self.enqueue(list_name, email)
     Delayed::Job.enqueue(new(list_name, email))
