42279432cab65f3e70ee9dcc8d73740de1e88cbf
Arun Agrawal <arunagw@gmail.com>
2013-07-22 11:45:15 +0200

    Turning on config.active_record.whitelist_attributes

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,12 @@ module Workshops
     # Don't connect to database when precompiling assets.
     # https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar#troubleshooting
     config.assets.initialize_on_precompile = false
+
+    # Enforce whitelist mode for mass assignment.
+    # This will create an empty whitelist of attributes available for mass-assignment for all models
+    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
+    # parameters by using an attr_accessible or attr_protected declaration.
+    config.active_record.whitelist_attributes = true
   end
 end
 
