8453a4b6163ba84ce51896b6bf1afc80f2612284
catalystmediastudios <catalystmediastudios@gmail.com>
2013-07-29 11:38:35 -0400

    User can update their bio from timeline
    * Add default `bio` text
    * Link to my account page
    * Add bio field to my account page
    * Add RSpec matcher to more easily test for data-roles

--- spec/requests/user_views_timeline_spec.rb
+++ spec/requests/user_views_timeline_spec.rb
@@ -21,6 +21,22 @@ feature 'User views their timeline', js: true do
     expect(page).to have_css '.timeline li', text: fake_trail_map.validation_title
   end
 
+  scenario 'they see their bio' do
+    user = create(:user, bio: 'All about me')
+
+    visit timeline_path(as: user)
+
+    expect(page).to have_role 'user-bio', text: 'All about me'
+  end
+
+  scenario 'they see a placeholder bio when none is present' do
+    user = create(:user, bio: nil)
+
+    visit timeline_path(as: user)
+
+    expect(page).to have_role 'user-bio', text: 'Tell us about yourself'
+  end
+
   private
 
   def fake_trail_map
