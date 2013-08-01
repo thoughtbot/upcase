5c4cfa7ce0bac8921e89030be4d13153f9027e77
catalystmediastudios <catalystmediastudios@gmail.com>
2013-08-01 11:19:15 -0400

    Add notes to the timeline, implement notes
    * Render notes on the left of the timeline
    * Removes notes from timeline items
    * Display notes on the left of the timeline
    * Show trails link only in newest week
    * Show note form only when clicked
    * Extract trails maps on timeline test

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,4 +3,8 @@ class Note < ActiveRecord::Base
 
   validates :body, presence: true
   validates :user_id, presence: true
+
+  def body_html
+    BlueCloth.new(body).to_html
+  end
 end
--- spec/requests/user_adds_a_note_to_timeline_spec.rb
+++ spec/requests/user_adds_a_note_to_timeline_spec.rb
@@ -1,13 +1,41 @@
 require 'spec_helper'
 
-feature 'User adds a note to timeline' do
+feature 'User adds a note to timeline', :js do
+  scenario 'they see the note form hidden until they click "Add a note"' do
+    user = create(:user)
+    visit timeline_path(as: user)
+
+    expect(page).to have_css '.add-note-form', visible: false
+    find('span', text: 'Add a note').click
+
+    expect(page).to have_css '.add-note-form', visible: true
+  end
+
   scenario 'they see the note on the timeline page' do
     user = create(:user)
     visit timeline_path(as: user)
 
-    fill_in 'note_body', with: 'I love to learn'
-    click_on 'Save'
+    create_note('I love to learn')
 
     expect(page).to have_role 'note', text: 'I love to learn'
+  end
+
+  scenario 'they see the note rendered as markdown' do
+    user = create(:user)
+    visit timeline_path(as: user)
+
+    create_note('# I love to learn')
+
+    expect(page).to have_css '[data-role="note"] span h1', text: 'I love to learn'
+  end
+
+  private
+
+  def create_note(body)
+    within '.left-column' do
+      find('span', text: 'Add a note').click
+      fill_in 'note_body', with: body
+      click_on 'Save'
+    end
   end
 end
