fdcb8a54c764cfcb6a965f7811e05e27aa757f2d
catalystmediastudios <catalystmediastudios@gmail.com>
2013-08-01 11:19:15 -0400

    Render note text using Markdown

--- spec/requests/user_adds_a_note_to_timeline_spec.rb
+++ spec/requests/user_adds_a_note_to_timeline_spec.rb
@@ -5,9 +5,24 @@ feature 'User adds a note to timeline' do
     user = create(:user)
     visit timeline_path(as: user)
 
-    fill_in 'note_body', with: 'I love to learn'
-    click_on 'Save'
+    create_note('I love to learn')
 
     expect(page).to have_role 'note', text: 'I love to learn'
+  end
+
+  scenario 'they see the note rendered as markdown' do
+    user = create(:user)
+    visit timeline_path(as: user)
+
+    create_note('# I love to learn')
+
+    expect(page).to have_css '[data-role="note"] span h1', text: 'I love to learn'
+  end
+
+  private
+
+  def create_note(body)
+    fill_in 'note_body', with: body
+    click_on 'Save'
   end
 end
