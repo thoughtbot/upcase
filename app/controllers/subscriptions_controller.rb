0fb879c3fce6a0601fb17ff88a87e16c1e345857
Chad Pytel <cpytel@thoughtbot.com>
2013-08-08 13:59:13 -0400

    Revert "new_subscription_path"
    This reverts commit ce9ba5f0cf81559a0a5f08c5579a501eea18a42c.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,7 +1,4 @@
 class SubscriptionsController < ApplicationController
-  def new
-  end
-
   def update
     customer = Stripe::Customer.retrieve(current_user.stripe_customer_id)
     customer.card = params['stripe_token']
