2c83564a95617a4b7ab55baff6f853c24aee3449
Arun Agrawal <arunagw@gmail.com>
2013-08-14 17:10:49 +0200

    Using github username when name is not present
    Using nickname from GitHub as few times
    name is not present in hash
    
    GitHub is also not a good name we are using
    name in emails
    
    Migration added to migrate old users
    
    closes #363

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130812194332) do
+ActiveRecord::Schema.define(:version => 20130814143800) do
 
   create_table "announcements", :force => true do |t|
     t.datetime "created_at",        :null => false
