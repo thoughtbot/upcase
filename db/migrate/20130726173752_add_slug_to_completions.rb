baf728b4d04a12cc2c4906fde8354140f165aba3
catalystmediastudios <catalystmediastudios@gmail.com>
2013-08-16 16:21:29 -0400

    Extract task for adding slug to old completions

--- db/migrate/20130726173752_add_slug_to_completions.rb
+++ db/migrate/20130726173752_add_slug_to_completions.rb
@@ -1,25 +1,5 @@
 class AddSlugToCompletions < ActiveRecord::Migration
-  def up
+  def change
     add_column :completions, :slug, :string
-
-    Completion.reset_column_information
-
-    Completion.all.each do |completion|
-      Trail.all.each do |trail|
-        if completion_is_part_of_trail?(completion, trail)
-          completion.update_attribute(:slug, trail.slug)
-        end
-      end
-    end
-  end
-
-  def down
-    remove_column :completions, :slug
-  end
-
-  private
-
-  def completion_is_part_of_trail?(completion, trail)
-    trail.resources_and_validations.detect { |item| item['id'] == completion.trail_object_id }
   end
 end
