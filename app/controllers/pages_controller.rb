2e0860149e2488e7c8db5ad23b7d8530131af055
Edwin <edwin.h.morris@gmail.com>
2013-08-20 16:13:01 -0400

    replace mentor visual with relevant info
    * add indications of scrollability
    * darken journey line
    * add numbers to features
    * reduce top margin on first feature
    * reduce widows at the 1280px width
    * remove faces of non-mentors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < HighVoltage::PagesController
   layout :layout_for_page
 
+  def show
+    @mentor_1 = User.mentors.first
+    @mentor_2 = User.mentors.second
+    render :template => current_page
+  end
+
   private
 
   def layout_for_page
