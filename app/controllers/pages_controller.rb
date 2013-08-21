82d89989ff63c757d5ab803d21bd3a94bc8cd69b
Chad Pytel <cpytel@thoughtbot.com>
2013-08-21 14:23:05 -0400

    fix broken specs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < HighVoltage::PagesController
   layout :layout_for_page
 
   def show
-    @mentor_1 = User.mentors.first
-    @mentor_2 = User.mentors.second
     render :template => current_page
   end
 
