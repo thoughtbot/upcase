8a61d5310aa46009fc902faf8b1aeca5b2a8437d
Melissa Xie <melissa@thoughtbot.com>
2013-08-29 20:14:21 -0400

    Allow mentors to write notes on mentee timelines

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -9,5 +9,15 @@ describe NotesController do
 
       expect(response).to redirect_to sign_in_path
     end
+
+    it "redirects non-admin user if they try to post on another's timeline" do
+      non_admin = create(:user, admin: false)
+      another_user = create(:user)
+      sign_in_as(non_admin)
+
+      post :create, note: { body: 'Hi', timeline_user_id: another_user.id }
+
+      expect(response).to redirect_to timeline_path
+    end
   end
 end
