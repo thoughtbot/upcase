70ca195d311ad1e78e8357c9baef85f83907222e
Ben Orenstein <ben.orenstein@gmail.com>
2013-09-04 17:02:48 -0400

    Fix migration. Null constraint requires a default.

--- db/migrate/20130903181127_make_plans_polymorphic.rb
+++ db/migrate/20130903181127_make_plans_polymorphic.rb
@@ -1,5 +1,5 @@
 class MakePlansPolymorphic < ActiveRecord::Migration
   def change
-    add_column :subscriptions, :plan_type, :string, null: false
+    add_column :subscriptions, :plan_type, :string, null: false, default: 'IndividualPlan'
   end
 end
