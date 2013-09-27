11c4d5b1e543de435472a3c2a2672b5d7fef66df
Chad Pytel <cpytel@thoughtbot.com>
2013-09-27 11:16:56 -0400

    fix for database with no plans

--- app/widgets/total_churn.rb
+++ app/widgets/total_churn.rb
@@ -1,16 +1,14 @@
-plans = [
-  IndividualPlan.prime_basic,
-  IndividualPlan.prime_workshops,
-  IndividualPlan.prime_with_mentoring,
-  TeamPlan.instance
-]
-
 widget :total_churn do
   key "24556fbc12af7ea8e01ba859c048152986722825"
   type "number_and_secondary"
   data do
     {
-      value: TotalChurn.new(plans).current
+      value: TotalChurn.new([
+        IndividualPlan.prime_basic,
+        IndividualPlan.prime_workshops,
+        IndividualPlan.prime_with_mentoring,
+        TeamPlan.instance
+      ]).current
     }
   end
 end
