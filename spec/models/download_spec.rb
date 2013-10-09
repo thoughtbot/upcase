616037e94ade4c9b87a22f58bdc67fdec54ffb1b
Arun Agrawal <arunagw@gmail.com>
2013-10-09 13:31:55 +0200

    Added specs for download.rb

---
+++ spec/models/download_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Download do
+
+  it 
+  describe '.display_name' do
+    it 'display name with file name and size' do
+      download = build_stubbed(:download,
+                               download_file_name: 'file-name',
+                               download_file_size: 100)
+
+      expect(download.display_name).to eq("file-name (100 Bytes)")
+    end
+  end
+end
