3e2365b4b5b30d594094c6394128b4ab20e85e1d
Arun Agrawal <arunagw@gmail.com>
2013-10-09 11:28:43 +0200

    Moved methods to ApiController

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,7 @@
 class Api::V1::UsersController < ApiController
   doorkeeper_for :all
 
-  respond_to :json
-
   def show
-    respond_with current_resource_owner
-  end
-
-  private
-
-  def current_resource_owner
-    User.find(doorkeeper_token.resource_owner_id)
+    respond_with resource_owner
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   helper :all
 
-  protect_from_forgery
+  protect_from_forgery with: :exception
 
   protected
 
