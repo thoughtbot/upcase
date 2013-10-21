ae3611f02b747157904cd59ecf41e6663aeccab1
Paul Smith <catalystmediastudios@gmail.com>
2013-10-21 16:31:52 -0400

    Remove iOS Smart Banner from trails
    Trajectory story:
    https://www.apptrajectory.com/thoughtbot/learn/stories/15636022

--- spec/requests/visitor_views_topic_spec.rb
+++ spec/requests/visitor_views_topic_spec.rb
@@ -1,29 +1,5 @@
 require 'spec_helper'
 
-feature 'Learn about the Learn iOS app' do
-  scenario 'a visitor sees a smart app banner on trails index and show' do
-    topic = create(:topic, name: 'Git', featured: true)
-
-    visit topics_path
-
-    expect(page).to have_css("meta[name='apple-itunes-app']")
-    expect(page.find("meta[name='apple-itunes-app']")["content"]).to(
-      include topics_url
-    )
-
-    click_link "Git"
-
-    expect(page).to have_css("meta[name='apple-itunes-app']")
-    expect(page.find("meta[name='apple-itunes-app']")["content"]).to(
-      include topic_url(topic)
-    )
-
-    visit root_path
-
-    expect(page).not_to have_css("meta[name='apple-itunes-app']")
-  end
-end
-
 feature 'Topic pages' do
   scenario 'a visitor views the list of featured topics' do
     create(:topic, :featured, name: 'Topic 1')
