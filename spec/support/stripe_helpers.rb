5c493ad897328acd5430cd179b3250b93aa0b328
Chad Pytel <cpytel@thoughtbot.com>
2013-11-03 11:26:18 -0500

    convert existing credit card feature to spec

--- spec/support/stripe_helpers.rb
+++ spec/support/stripe_helpers.rb
@@ -8,4 +8,15 @@ module StripeHelpers
     fill_in 'Email', with: 'ben@thoughtbot.com'
     click_button 'Submit Payment'
   end
+
+  def pay_using_stripe_with_existing_card
+    find(:css, "input.use_existing_card").set(true)
+    fill_in 'Name', with: 'Ben'
+    fill_in 'Email', with: 'ben@thoughtbot.com'
+    click_button 'Submit Payment'
+  end
+
+  def stub_stripe_to_fail
+    FakeStripe.failure = true
+  end
 end
