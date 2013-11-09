be5cb6d6ba26732be4499a19bdc77bd4bc16ceb2
Chad Pytel <cpytel@thoughtbot.com>
2013-11-09 15:16:25 -0500

    Convert products/test_driven_rails_resources.feature to rspec

---
+++ spec/requests/test_driven_rails_resources_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+feature 'Test-Driven Rails Resources' do
+  scenario 'View the resources' do
+    visit '/test-driven-rails-resources'
+
+    expect(page).to have_css('a[href*="0132350882"]')
+  end
+end
