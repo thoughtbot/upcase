b0f2d31b368ac2cbe7de97e7242363a3205252b5
Ben Orenstein <ben.orenstein@gmail.com>
2013-11-09 14:13:08 -0500

    Add widget for showing trailing 30 days of cancellations.

--- app/models/metrics.rb
+++ app/models/metrics.rb
@@ -6,4 +6,8 @@ class Metrics
   def subscription_signups_since(date)
     @subscription_relation.where(created_at: (date..Time.zone.now))
   end
+
+  def canceled_subscriptions_since(date)
+    @subscription_relation.where(deactivated_on: (date..Time.zone.today))
+  end
 end
--- spec/models/metrics_spec.rb
+++ spec/models/metrics_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 describe Metrics do
   describe '#subscription_signups_since' do
     it 'returns subscriptions created after the date provided' do
-      subscription_relation = stub('subscription relation', where: nil)
-
       Timecop.freeze do
         date = 30.days.ago.to_date
         Metrics.new(subscription_relation).subscription_signups_since(date)
@@ -12,5 +10,20 @@ describe Metrics do
         expect(subscription_relation).to have_received(:where).with(created_at: (date..Time.zone.now))
       end
     end
+  end
+
+  describe '#canceled_subscriptions_since' do
+    it 'returns the subscriptions that have been canceled since a given date' do
+      Timecop.freeze do
+        date = 30.days.ago.to_date
+        Metrics.new(subscription_relation).canceled_subscriptions_since(date)
+
+        expect(subscription_relation).to have_received(:where).with(deactivated_on: (date..Time.zone.today))
+      end
+    end
+  end
+
+  def subscription_relation
+    @subscription_relation ||= stub('subscription relation', where: nil)
   end
 end
