7da3430f030c93e19aad82e3d665be1b1c9f5cdc
Ben Orenstein <ben.orenstein@gmail.com>
2013-11-15 14:56:53 -0500

    Fix too many mentors on landing

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,9 +1,10 @@
 class Mentor
+  NUMBER_OF_MENTORS_TO_FEATURE = 2
   def initialize(relation = User)
     @relation = relation
   end
 
   def featured
-    @relation.mentors.sample(5)
+    @relation.mentors.sample(NUMBER_OF_MENTORS_TO_FEATURE)
   end
 end
