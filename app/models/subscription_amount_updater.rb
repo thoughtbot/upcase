8ef14a3dc746f15d85e81f4a65f8bc477f04ec09
Ben Orenstein <ben.orenstein@gmail.com>
2013-12-04 11:48:26 -0500

    Remove error handling

--- app/models/subscription_amount_updater.rb
+++ app/models/subscription_amount_updater.rb
@@ -15,22 +15,9 @@ class SubscriptionAmountUpdater
 
   def next_invoice_total(stripe_customer_id)
     next_invoice(stripe_customer_id).total
-  rescue Stripe::InvalidRequestError => error
-    notify_airbrake(error)
-    0
   end
 
   def next_invoice(stripe_customer_id)
     Stripe::Invoice.upcoming(customer: stripe_customer_id)
-  end
-
-  def notify_airbrake(error)
-    unless error_is_because_user_has_no_upcoming_invoice?(error)
-      Airbrake.notify(error)
-    end
-  end
-
-  def error_is_because_user_has_no_upcoming_invoice?(error)
-    error.http_status == 404
   end
 end
--- spec/models/subscription_amount_updater_spec.rb
+++ spec/models/subscription_amount_updater_spec.rb
@@ -15,32 +15,6 @@ describe SubscriptionAmountUpdater do
     expect(subscription.reload.next_payment_amount).to eq 10
   end
 
-  it 'sets the next_payment_amount to 0 when it is 404' do
-    Stripe::Invoice.stubs(:upcoming).raises(
-      Stripe::InvalidRequestError.new('No upcoming invoices for customer', '', 404)
-    )
-    subscription = create(:subscription)
-    subscriptions = [subscription]
-
-    SubscriptionAmountUpdater.new(subscriptions).process
-
-    expect(subscription.reload.next_payment_amount).to eq 0
-  end
-
-  it "sends the error to Airbrake if it isn't 404" do
-    Airbrake.stubs(:notify)
-    error = Stripe::InvalidRequestError.new('Server error', '', 500)
-    Stripe::Invoice.stubs(:upcoming).raises(
-      error
-    )
-    subscription = create(:subscription)
-    subscriptions = [subscription]
-
-    SubscriptionAmountUpdater.new(subscriptions).process
-
-    expect(Airbrake).to have_received(:notify).with(error)
-  end
-
   private
 
   def stub_stripe_invoice_with_total(amount)
