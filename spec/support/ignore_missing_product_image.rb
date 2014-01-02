07484e1edb527107f02eaa934c43dd5a11569251
Prem Sichanugrist <s@sikac.hu>
2014-01-02 17:32:24 +0700

    Also ignore missing workshop thumbnail

--- spec/support/ignore_missing_product_image.rb
+++ spec/support/ignore_missing_product_image.rb
@@ -8,7 +8,7 @@ class IgnoreMissingProductImage
   def call(env)
     @app.call(env)
   rescue ActionController::RoutingError => e
-    if e.message =~ %r{product_images/.+\.(png|jpg)}
+    if e.message =~ %r{(?:product_images|workshop_thumbs)/.+\.(png|jpg)}
       [200, { 'Content-Type' => "image/#{$1}", 'Content-Length' => 0 }, ['']]
     else
       raise e
