ba8f36d95b3e7a579b3edb64461df6e7eb94b50c
Joe Ferris <jferris@thoughtbot.com>
2014-01-20 14:15:13 -0500

    Redirect to dashboard with nil Omniauth origin
    Omniauth sometimes fails to pass an origin URL. Redirect to the
    dashboard when this is the case.
    
    https://github.com/intridea/omniauth/issues/306

---
+++ spec/controllers/auth_callbacks_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe AuthCallbacksController do
+  context '#create' do
+    it 'redirects to the dashboard path without an auth origin' do
+      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
+      request.env['omniauth.origin'] = nil
+
+      get :create, provider: 'github'
+
+      should redirect_to(dashboard_url)
+    end
+  end
+end
