f2cc9896002b21c8d8d9bc79f43fcc52cee72bd5
Joe Ferris <jferris@thoughtbot.com>
2014-01-23 09:58:31 -0500

    Update TeamPlan name on polymorphic relationships
    * We renamed TeamPlan to Teams::TeamPlan
    * Two tables had polymorphic relationships that included TeamPlan
    * The *_type columns for TeamPlan needed to be updated
    
    https://www.apptrajectory.com/thoughtbot/learn/stories/15639570

---
+++ db/migrate/20140123145010_update_team_plans_type.rb
@@ -0,0 +1,21 @@
+class UpdateTeamPlansType < ActiveRecord::Migration
+  def up
+    update_type :subscriptions, :plan_type, 'TeamPlan', 'Teams::TeamPlan'
+    update_type :purchases, :purchaseable_type, 'TeamPlan', 'Teams::TeamPlan'
+  end
+
+  def down
+    update_type :subscriptions, :plan_type, 'Teams::TeamPlan', 'TeamPlan'
+    update_type :purchases, :purchaseable_type, 'Teams::TeamPlan', 'TeamPlan'
+  end
+
+  def update_type(table, column, previous, new)
+    say_with_time "Changing #{previous} to #{new} on #{table}.#{column}" do
+      connection.update(<<-SQL)
+        UPDATE #{table}
+        SET #{column} = '#{new}'
+        WHERE #{column} = '#{previous}'
+      SQL
+    end
+  end
+end
