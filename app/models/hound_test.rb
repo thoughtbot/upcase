19849172109ed22abdc6f50c5c1972085eb5dac4
Joe Ferris <jferris@thoughtbot.com>
2014-02-06 13:45:21 -0500

    Hound test

---
+++ app/models/hound_test.rb
@@ -0,0 +1,5 @@
+class HoundTest
+  def method
+    this && is && a && really && long && line && and && I && hope && that && hound && will && complain && about && it && because && lets && be && honest && thats && its && job && you && know
+  end
+end
