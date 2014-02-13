3db2b5003196b4295d4bbd0f5bf44e4d452fbedd
Arun Agrawal <arunagw@gmail.com>
2014-02-13 14:11:27 +0100

    Moving into controlled specs

--- spec/features/purchases_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'Purchases' do
-  it 'raise not found if no purchase exists' do
-    visit purchase_path('robots.txt')
-    expect(page.status_code).to eq(404)
-  end
-end
