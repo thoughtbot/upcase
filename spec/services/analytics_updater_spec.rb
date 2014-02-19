78c50a1ccfdbc6bf79a3e0fee32673aa2f08588c
Arun Agrawal <arunagw@gmail.com>
2014-02-19 16:25:25 +0100

    Using ClimateControl

--- spec/services/analytics_updater_spec.rb
+++ spec/services/analytics_updater_spec.rb
@@ -3,22 +3,26 @@ require 'spec_helper'
 describe AnalyticsUpdater do
   describe 'unsubscribe' do
     let(:user) { create(:user) }
-    let(:key) { ENV['SEGMENT_KEY'] }
+
+    around(:each) do |example|
+      ClimateControl.modify(
+        SEGMENT_KEY: segment_key,
+        INTERCOM_API_SECRET: intercom_secret) do
+          example.run
+        end
+    end
 
     before :each do
-      AnalyticsRuby.stubs(:init).with(secret: key)
+      AnalyticsRuby.stubs(:init).with(secret: segment_key)
       traits = { has_active_subscription: false }.merge(intercom_hash)
       AnalyticsRuby.stubs(:identify).with(user_id: user.id.to_s, traits: traits)
     end
 
     describe 'analytics enable' do
-      before :each do
-        @old_analytics = ENV['ANALYTICS']
-        ENV['ANALYTICS'] = 'on'
-      end
-
-      after :each do
-        ENV['ANALYTICS'] = @old_analytics
+      around(:each) do |example|
+        ClimateControl.modify ANALYTICS: 'on' do
+          example.run
+        end
       end
 
       it 'should unsubscribe user' do
@@ -26,21 +30,20 @@ describe AnalyticsUpdater do
         updater.unsubscribe
 
         expect(AnalyticsRuby).
-          to have_received(:init).with(secret: key)
+          to have_received(:init).with(secret: segment_key)
         expect(AnalyticsRuby).
-          to have_received(:identify).with(user_id: user.id.to_s, traits: { has_active_subscription: false }.
-                                           merge(intercom_hash))
+          to have_received(:identify).with(
+            user_id: user.id.to_s,
+            traits: { has_active_subscription: false }.merge(intercom_hash)
+        )
       end
     end
 
     describe 'analytics disabled' do
-      before :each do
-        @old_analytics = ENV['ANALYTICS']
-        ENV['ANALYTICS'] = nil
-      end
-
-      after :each do
-        ENV['ANALYTICS'] = @old_analytics
+      around(:each) do |example|
+        ClimateControl.modify ANALYTICS: nil do
+          example.run
+        end
       end
 
       it 'should not update analytics if disabled' do
@@ -60,9 +63,17 @@ describe AnalyticsUpdater do
       'Intercom' => {
         userHash: OpenSSL::HMAC.hexdigest(
           'sha256',
-          ENV['INTERCOM_API_SECRET'],
+          intercom_secret,
           user.id.to_s)
       }
     }
+  end
+
+  def segment_key
+    'segment-key'
+  end
+
+  def intercom_secret
+    'secret'
   end
 end
