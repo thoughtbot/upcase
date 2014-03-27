9ed222f87201637286668bef41d23e2b827f54e1
Calle Erlandsson <calle@thoughtbot.com>
2014-03-27 13:58:54 +0100

    Only count published episodes

--- spec/views/shows/_show.html.erb_spec.rb
+++ spec/views/shows/_show.html.erb_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe 'shows/_show.html.erb' do
-  it 'includes episodes count' do
+  it 'includes published episodes count' do
     show = create(:show)
     create_list(:video, 2, watchable: show)
+    create(:video, :unpublished, watchable: show)
 
     render 'shows/show', show: show
 
