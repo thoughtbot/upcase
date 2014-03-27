67a142328a4aaf07967f0b2aca28db4ff985d418
Calle Erlandsson <calle@thoughtbot.com>
2014-03-27 14:15:03 +0100

    Test video count for screencasts

---
+++ spec/views/screencasts/_screencast.html.erb_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'shows/_screencast.html.erb' do
+  it 'includes published episodes count' do
+    screencast = create(:screencast)
+    create_list(:video, 2, watchable: screencast)
+
+    render 'screencasts/screencast', screencast: screencast
+
+    expect(rendered).to include('2 videos')
+  end
+end
