440da526cf801ae40d26b07754bc66fec81a8741
Calle Erlandsson <calle@thoughtbot.com>
2014-04-02 15:10:29 +0200

    Don't show mentor with canceled subscription
    This ensures that the "Contact your mentor" link is not visible for a user with
    a mentoring plan after the plan has been canceled.
    
    https://www.apptrajectory.com/thoughtbot/learn/stories/15642101

--- spec/features/subscriber_contacts_mentor_spec.rb
+++ spec/features/subscriber_contacts_mentor_spec.rb
@@ -5,6 +5,9 @@ feature 'Subscriber can contact his mentor' do
     sign_in_as_user_with_mentoring_subscription
     mentor = @current_user.mentor
 
-    expect(page).to have_content(I18n.t('dashboards.show.contact_your_mentor', mentor_name: mentor.first_name))
+    expect(page).to have_content(I18n.t(
+      'dashboards.show.contact_your_mentor',
+      mentor_name: mentor.first_name
+    ))
   end
 end
