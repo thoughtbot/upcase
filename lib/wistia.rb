1b24bd84d1756f447f8fa22524f2050ead587a2e
Joël Quenneville <joelq@thoughtbot.com>
2014-05-13 11:50:42 -0400

    Revert changes to Wistia
    Reverts changes to the Wistia class made in
    ba5e82680807d8b8f139b15ec7b3d43501dffaef which resulted in some parsing
    errors.
    
    Removed test for `Wistia.get_media_hash` which no longer tested anything
    useful.

--- lib/wistia.rb
+++ lib/wistia.rb
@@ -4,7 +4,7 @@ class Wistia
   basic_auth 'api', ENV['WISTIA_API_KEY']
 
   def self.get_media_hash_from_id id
-    get("/medias/#{id}.json").body
+    get("/medias/#{id}.json")
   rescue *HTTP_ERRORS => e
     Airbrake.notify e
     {}
