b89becfa4be83a7499dfc2d9cbeefb31916cf909
Ben Orenstein <ben.orenstein@gmail.com>
2014-06-19 14:01:39 -0400

    Visit the purchase path directly in TeamPlan specs
    Rather than navigating from the plans page, just visit the purchase path
    directly.

--- spec/support/features/team_plan_helpers.rb
+++ spec/support/features/team_plan_helpers.rb
@@ -1,7 +1,6 @@
 module Features
   def visit_team_plan_purchase_page
     plan = create(:team_plan)
-    visit new_subscription_path
-    click_link plan.name
+    visit new_teams_team_plan_purchase_path(plan)
   end
 end
