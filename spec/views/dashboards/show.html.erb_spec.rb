a14a003bc0f55d13d8318aa0949e9af7096d0761
Melanie Gilman <melanie@thoughtbot.com>
2014-06-24 16:50:49 -0400

    Conditionally hide screencasts from dashboard
    Only show screencasts to users whose subscriptions include them.
    
    https://trello.com/c/lfd35vz3

--- spec/views/dashboards/show.html.erb_spec.rb
+++ spec/views/dashboards/show.html.erb_spec.rb
@@ -1,10 +1,10 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe 'dashboards/show.html' do
-  ENV['LEARN_REPO_URL'] = 'learn_repo_url'
+describe "dashboards/show.html" do
+  ENV["LEARN_REPO_URL"] = "learn_repo_url"
 
-  context 'when a user does not have access to the learn repo' do
-    it 'renders without a link to learn repo' do
+  context "when a user does not have access to the learn repo" do
+    it "renders without a link to learn repo" do
       render_show
 
       expect(rendered).
@@ -13,8 +13,8 @@ describe 'dashboards/show.html' do
     end
   end
 
-  context 'when a user has access to the learn repo' do
-    it 'renders with a link to learn repo' do
+  context "when a user has access to the learn repo" do
+    it "renders with a link to learn repo" do
       render_show source_code: true
 
       expect(rendered).
@@ -22,8 +22,8 @@ describe 'dashboards/show.html' do
     end
   end
 
-  context 'when a user does not have access to learn live' do
-    it 'renders without a link to learn live' do
+  context "when a user does not have access to learn live" do
+    it "renders without a link to learn live" do
       render_show
 
       expect(rendered).not_to have_css("a[href='#{OfficeHours.url}']")
@@ -31,37 +31,46 @@ describe 'dashboards/show.html' do
     end
   end
 
-  context 'when a user has access to learn live' do
-    it 'renders with a link to learn live' do
+  context "when a user has access to learn live" do
+    it "renders with a link to learn live" do
       render_show office_hours: true
 
       expect(rendered).to have_css("a[href='#{OfficeHours.url}']")
     end
   end
 
-  it 'renders with access to workshops and shows' do
-    render_show shows: true, workshops: true, exercises: true
+  context "when a user has access to exercises, workshops, and shows" do
+    it "renders with access to exercises, workshops and shows" do
+      render_show shows: true, workshops: true, exercises: true
 
-    expect(rendered).not_to have_content('locked')
+      expect(rendered).not_to have_content("locked")
+    end
   end
 
-  it 'renders with access to shows but without access to workshops' do
-    render_show shows: true, workshops: true
+  context "when a user has access to shows and workshops" do
+    it "renders with access to shows but without access to workshops" do
+      render_show shows: true, workshops: true
 
-    expect(rendered).to have_content('Exercises are locked')
+      expect(rendered).to have_content("Exercises are locked")
+    end
   end
 
-  it 'renders with access to shows but without access to workshops' do
-    render_show shows: true
+  context "when a user has access to shows" do
+    it "renders with access to shows but without access to workshops" do
+      render_show shows: true
 
-    expect(rendered).to have_content('Exercises and workshops are locked')
+      expect(rendered).
+        to have_content("Exercises and video tutorials are locked")
+    end
   end
 
-  it 'renders without access to shows or workshops' do
-    render_show
+  context "when a user does not have access to shows" do
+    it "renders without access to shows, workshops, and exercises" do
+      render_show
 
-    expect(rendered).
-      to have_content('Exercises, workshops, and shows are locked')
+      expect(rendered).
+        to have_content("Exercises, video tutorials, and shows are locked")
+    end
   end
 
   def render_show(options = {})
@@ -73,7 +82,7 @@ describe 'dashboards/show.html' do
     view_stubs(:current_user_has_access_to_shows?).
       returns(options[:shows])
     assign :catalog, Catalog.new
-    render template: 'dashboards/show'
+    render template: "dashboards/show"
   end
 
   def default_options
@@ -81,6 +90,7 @@ describe 'dashboards/show.html' do
       source_code: false,
       office_hours: false,
       forum: false,
+      screencasts: false,
       shows: false,
       workshops: false,
       exercises: false
