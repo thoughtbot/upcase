cfc742b8c8f4ee64e7c4f3373445e7fed2c8deef
Arun Agrawal <arunagw@gmail.com>
2014-06-27 15:04:32 +0200

    Hound?

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'sprockets/railtie'
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # If you have a Gemfile, require the default gems, the ones in the
