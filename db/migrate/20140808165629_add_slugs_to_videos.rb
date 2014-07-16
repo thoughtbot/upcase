6fda956193f1a8958a01da074e899d73e2680869
Chad Pytel <chad@thoughtbot.com>
2014-07-16 13:14:50 -0400

    Add slugs to videos
    https://trello.com/c/LSPx3QqK/97-use-friendly-ids-for-videos

---
+++ db/migrate/20140808165629_add_slugs_to_videos.rb
@@ -0,0 +1,17 @@
+class AddSlugsToVideos < ActiveRecord::Migration
+  def change
+    add_column :videos, :slug, :string, null: true
+
+    videos = select_all("select id, title from videos")
+    videos.each do |video|
+      update(<<-SQL)
+        UPDATE videos
+          SET slug='#{video["title"].parameterize}'
+          WHERE id=#{video["id"]}
+      SQL
+    end
+
+    change_column_null :videos, :slug, false
+    add_index :videos, :slug, unique: true
+  end
+end
