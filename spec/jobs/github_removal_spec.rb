f5e33c034a37f711d5afd1b7d834d4837a7f3ff1
Chad Pytel <chad@thoughtbot.com>
2014-07-25 15:58:08 -0400

    fix jobs/github_removal_spec.rb

--- spec/jobs/github_removal_spec.rb
+++ spec/jobs/github_removal_spec.rb
@@ -1,16 +1,15 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe GithubRemovalJob do
-  it_behaves_like 'a Delayed Job that notifies Airbrake about errors'
+  it_behaves_like "a Delayed Job that notifies Airbrake about errors"
 
-  it 'removes a username from a github team' do
+  it "removes a username from a github team" do
     client = stub_octokit
     client.stubs(:remove_team_member => nil)
 
-    GithubRemovalJob.new(3, ['gabebw', 'cpytel']).perform
+    GithubRemovalJob.new(3, "gabebw").perform
 
-    client.should have_received(:remove_team_member).with(3, 'gabebw')
-    client.should have_received(:remove_team_member).with(3, 'cpytel')
+    client.should have_received(:remove_team_member).with(3, "gabebw")
   end
 
   [Octokit::NotFound, Net::HTTPBadResponse].each do |error_class|
@@ -19,14 +18,14 @@ describe GithubRemovalJob do
       client.stubs(:remove_team_member).raises(error_class)
       Airbrake.stubs(:notify)
 
-      GithubRemovalJob.new(3, ['gabebw']).perform
+      GithubRemovalJob.new(3, "gabebw").perform
 
       Airbrake.should have_received(:notify).with(instance_of(error_class))
     end
   end
 
   def stub_octokit
-    stub('Octokit::Client').tap do |client|
+    stub("Octokit::Client").tap do |client|
       Octokit::Client.stubs(:new =>  client)
     end
   end
