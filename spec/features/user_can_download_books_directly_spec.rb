a0546a2ec46da7c13c6d52fa14b0ddc819fd1180
Chad Pytel <chad@thoughtbot.com>
2014-07-25 16:27:33 -0400

    fix user_can_download_books_directly_spec

--- spec/features/user_can_download_books_directly_spec.rb
+++ spec/features/user_can_download_books_directly_spec.rb
@@ -4,13 +4,12 @@ feature 'User can download books directly' do
   scenario 'User sees links for various book formats' do
     user = create(:user)
     product = create(:book, :github, name: 'test github')
-    create(:purchase, user: user, purchaseable: product)
+    create(:license, user: user, licenseable: product)
 
     visit product_path(product, as: user)
 
-    page.should have_css("a[href*='test-github.pdf']")
-    page.should have_css("a[href*='test-github.epub']")
-    page.should have_css("a[href*='test-github.mobi']")
+    expect(page).to have_css("a[href*='test-github.pdf']")
+    expect(page).to have_css("a[href*='test-github.epub']")
+    expect(page).to have_css("a[href*='test-github.mobi']")
   end
-
 end
