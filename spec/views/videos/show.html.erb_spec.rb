3ec46192dfff390ca06cff99fdea30785319d148
Chad Pytel <chad@thoughtbot.com>
2014-07-25 16:54:16 -0400

    fix spec/views/videos/show.html.erb_spec

--- spec/views/videos/show.html.erb_spec.rb
+++ spec/views/videos/show.html.erb_spec.rb
@@ -1,26 +1,26 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe 'episodes/show' do
-  it 'embeds a preview when available' do
+describe "videos/show" do
+  it "embeds a preview when available" do
     video = build(
       :video,
       :published,
       watchable: build_stubbed(:show),
-      preview_wistia_id: '123456'
+      preview_wistia_id: "123456"
     )
 
-    wistia_id = '123'
+    wistia_id = "123"
     preview_video = Clip.new(wistia_id)
     video.stubs(:preview).returns(preview_video)
     stub_controller(video)
 
-    render template: 'episodes/show'
+    render template: "videos/show"
 
     expect(rendered).
       to have_css("p.videowrapper[data-wistia-id='#{wistia_id}']")
   end
 
-  it 'shows a thumbnail when there is no preview' do
+  it "shows a thumbnail when there is no preview" do
     video = build(
       :video,
       :published,
@@ -28,13 +28,13 @@ describe 'episodes/show' do
       preview_wistia_id: nil
     )
 
-    wistia_id = '123'
+    wistia_id = "123"
     clip = stub(wistia_id: wistia_id)
     thumbnail = VideoThumbnail.new(clip)
     video.stubs(:preview).returns(thumbnail)
     stub_controller(video)
 
-    render template: 'episodes/show'
+    render template: "videos/show"
 
     expect(rendered).to have_css("img.thumbnail[data-wistia-id='#{wistia_id}']")
   end
@@ -45,5 +45,7 @@ describe 'episodes/show' do
     assign :video, video
 
     view_stubs(:signed_out?).returns(true)
+    view_stubs(:signed_in?).returns(false)
+    view_stubs(:current_user_has_active_subscription?).returns(false)
   end
 end
