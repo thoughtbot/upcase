e9c5ae07d778309654bd353280626506a041c79f
Chad Pytel <chad@thoughtbot.com>
2014-07-26 14:13:20 -0400

    fix modules/teams/team_fulfillment_spec

--- app/modules/teams/team_fulfillment.rb
+++ app/modules/teams/team_fulfillment.rb
@@ -1,7 +1,7 @@
 module Teams
   class TeamFulfillment
-    def initialize(purchase, user)
-      @purchase = purchase
+    def initialize(checkout, user)
+      @checkout = checkout
       @user = user
     end
 
@@ -16,7 +16,7 @@ module Teams
       Team.create!(
         name: generate_team_name,
         subscription: subscription,
-        max_users: @purchase.quantity
+        max_users: @checkout.quantity
       )
     end
 
--- spec/modules/teams/team_fulfillment_spec.rb
+++ spec/modules/teams/team_fulfillment_spec.rb
@@ -9,15 +9,15 @@ module Teams
           :with_subscription,
           email: 'user.name@whatever.somethingcool.com'
         )
-        purchase = build_stubbed(:purchase, quantity: 4)
+        checkout = build_stubbed(:checkout, quantity: 4)
 
-        TeamFulfillment.new(purchase, user).fulfill
+        TeamFulfillment.new(checkout, user).fulfill
 
         team = user.reload.team
         expect(team).to be_present
         expect(team.name).to eq('Somethingcool')
         expect(team.subscription).to eq(user.subscription)
-        expect(team.max_users).to eq(purchase.quantity)
+        expect(team.max_users).to eq(checkout.quantity)
       end
     end
   end
