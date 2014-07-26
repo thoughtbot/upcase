b484d3b6e628ad24d917aafcfb1fff5d87c3a89d
Chad Pytel <chad@thoughtbot.com>
2014-07-26 14:13:40 -0400

    fix quotes in modules/teams/team_fulfillment_spec

--- spec/modules/teams/team_fulfillment_spec.rb
+++ spec/modules/teams/team_fulfillment_spec.rb
@@ -1,13 +1,13 @@
-require 'spec_helper'
+require "spec_helper"
 
 module Teams
   describe TeamFulfillment do
-    describe '#fulfill' do
-      it 'creates a new team with just that user' do
+    describe "#fulfill" do
+      it "creates a new team with just that user" do
         user = create(
           :user,
           :with_subscription,
-          email: 'user.name@whatever.somethingcool.com'
+          email: "user.name@whatever.somethingcool.com"
         )
         checkout = build_stubbed(:checkout, quantity: 4)
 
@@ -15,7 +15,7 @@ module Teams
 
         team = user.reload.team
         expect(team).to be_present
-        expect(team.name).to eq('Somethingcool')
+        expect(team.name).to eq("Somethingcool")
         expect(team.subscription).to eq(user.subscription)
         expect(team.max_users).to eq(checkout.quantity)
       end
