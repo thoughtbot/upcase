c60bee3c38059225f8c2d6c2b0964ae1b8440314
Chad Pytel <chad@thoughtbot.com>
2014-07-28 21:51:07 -0400

    update schema for migration from master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140725142656) do
+ActiveRecord::Schema.define(version: 20140727180715) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "pg_stat_statements"
