b5783f1e9708da52fb84b0058fc86395aeb7953b
Chad Pytel <chad@thoughtbot.com>
2014-07-29 11:10:43 -0400

    move support email into ENV variable

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,4 @@
 Clearance.configure do |config|
-  config.mailer_sender = 'thoughtbot <learn@thoughtbot.com>'
+  config.mailer_sender = "Upcase <#{ENV["SUPPORT_EMAIL"]}>"
   config.password_strategy = Clearance::PasswordStrategies::SHA1
 end
