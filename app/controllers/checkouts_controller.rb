9d6a5622a651987e19147129bb596b2a919453c9
Chad Pytel <chad@thoughtbot.com>
2014-07-30 09:51:51 -0400

    remove CheckoutBuilder

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -11,12 +11,9 @@ class CheckoutsController < ApplicationController
   end
 
   def create
-    @checkout =
-      CheckoutBuilder.
-        new(params: params,
-            user: current_user,
-            checkouts_collection: requested_subscribeable.checkouts).
-        build
+    @checkout = requested_subscribeable.checkouts.build(checkout_params)
+    @checkout.user = current_user
+    @checkout.stripe_customer_id = existing_stripe_customer_id
     CheckoutPrepopulator.new(@checkout, current_user).prepopulate_with_user_info
 
     if @checkout.save
@@ -58,5 +55,35 @@ class CheckoutsController < ApplicationController
 
   def url_after_denied_access_when_signed_out
     sign_up_url
+  end
+
+  def checkout_params
+    params.
+      require(:checkout).
+      permit(:stripe_coupon_id,
+             :name,
+             :email,
+             :password,
+             :github_username,
+             :organization,
+             :address1,
+             :address2,
+             :city,
+             :state,
+             :zip_code,
+             :country,
+             :payment_method,
+             :stripe_token,
+             :quantity)
+  end
+
+  def existing_stripe_customer_id
+    if using_existing_card?
+      current_user.stripe_customer_id
+    end
+  end
+
+  def using_existing_card?
+    params[:use_existing_card] == 'on'
   end
 end
