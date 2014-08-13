26ccdb0ae8155d2769d441915eae522daffe5561
Arun Agrawal <arunagw@gmail.com>
2014-08-13 15:06:06 +0200

    Migration to change PlanType

---
+++ db/migrate/20140813130414_update_plan_type.rb
@@ -0,0 +1,21 @@
+class UpdatePlanType < ActiveRecord::Migration
+  def up
+    update_type :subscriptions, :plan_type, 'Teams::TeamPlan', 'TeamPlan'
+  end
+
+  def down
+    update_type :subscriptions, :plan_type, 'TeamPlan', 'Teams::TeamPlan'
+  end
+
+  private
+
+  def update_type(table, column, previous, new)
+    say_with_time "Changing #{previous} to #{new} on #{table}.#{column}" do
+      connection.update(<<-SQL)
+        UPDATE #{table}
+        SET #{column} = '#{new}'
+        WHERE #{column} = '#{previous}'
+      SQL
+    end
+  end
+end
