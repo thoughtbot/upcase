97e59fd9228a729f2044b20be473fab77de7e4c1
Joe Ferris <jferris@thoughtbot.com>
2014-09-02 15:40:01 -0400

    Update version in db/schema.rb
    * Migration db/migrate/20140827165919_remove_teams_max_users.rb was added
    * The version in db/schema.rb was not updated
    * Rails gets confused about which migrations have run
    
    https://circleci.com/gh/thoughtbot/upcase/4230

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826203235) do
+ActiveRecord::Schema.define(version: 20140827165919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
