336b8ff476809f327c70ac633cbac2f96ec5af39
Tute Costa <tutecosta@gmail.com>
2014-09-08 11:37:54 -0400

    Add missing migration for Workshop rename

---
+++ db/migrate/20140908153238_update_polymorphic_relations.rb
@@ -0,0 +1,47 @@
+class UpdatePolymorphicRelations < ActiveRecord::Migration
+  def up
+    update_polymorphic_references :classifications,
+                                  :classifiable_type,
+                                  "Workshop",
+                                  "VideoTutorial"
+
+    update_polymorphic_references :licenses,
+                                  :licenseable_type,
+                                  "Workshop",
+                                  "VideoTutorial"
+
+    update_polymorphic_references :videos,
+                                  :watchable_type,
+                                  "Workshop",
+                                  "VideoTutorial"
+  end
+
+  def down
+    update_polymorphic_references :classifications,
+                                  :classifiable_type,
+                                  "VideoTutorial",
+                                  "Workshop"
+
+    update_polymorphic_references :licenses,
+                                  :licenseable_type,
+                                  "VideoTutorial",
+                                  "Workshop"
+
+    update_polymorphic_references :videos,
+                                  :watchable_type,
+                                  "VideoTutorial",
+                                  "Workshop"
+  end
+
+  private
+
+  def update_polymorphic_references(table, column, previous, new)
+    say_with_time "Changing #{previous} to #{new} on #{table}.#{column}" do
+      connection.update(<<-SQL)
+        UPDATE #{table}
+        SET #{column} = '#{new}'
+        WHERE #{column} = '#{previous}'
+      SQL
+    end
+  end
+end
