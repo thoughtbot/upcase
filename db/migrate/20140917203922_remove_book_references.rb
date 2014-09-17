175df7c322c011b6b0aa3cd9b2ccb6aa5dcb0da2
Tute Costa <tutecosta@gmail.com>
2014-09-17 16:43:42 -0400

    Remove Book polymorphic references
    Book doesn't exist any longer, and Rails tries to initialize from an inexisting
    constant.

---
+++ db/migrate/20140917203922_remove_book_references.rb
@@ -0,0 +1,27 @@
+class RemoveBookReferences < ActiveRecord::Migration
+  def up
+    remove_book_references_from(:classifications, :classifiable_type, :classifiable_id)
+    remove_book_references_from(:downloads, :purchaseable_type, :purchaseable_id)
+    remove_book_references_from(:licenses, :licenseable_type, :licenseable_id)
+    delete("DELETE FROM products WHERE type = 'Book'")
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+
+  private
+
+  def remove_book_references_from(table, type_column, id_column)
+    say_with_time "Removing Book references from #{table}.#{type_column}" do
+      # When referenced as Products
+      delete <<-SQL
+        DELETE FROM #{table}
+          USING products
+          WHERE products.type = 'Book'
+            AND (#{type_column} = products.type OR #{type_column} = 'Product')
+            AND #{id_column} = products.id
+      SQL
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917182800) do
+ActiveRecord::Schema.define(version: 20140917203922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
