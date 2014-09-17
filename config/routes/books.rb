ac5b897fa471f653775234ddc98213dd72555523
Caleb Thompson <caleb@calebthompson.io>
2014-09-17 12:45:04 -0500

    Redirect book paths

--- config/routes/books.rb
+++ config/routes/books.rb
@@ -7,3 +7,9 @@ get(
   as: :book,
   constraints: LicenseableConstraint.new(Book)
 )
+
+get '/backbone-js-on-rails' => redirect('https://gumroad.com/l/backbone-js-on-rails')
+get '/geocoding-on-rails' => redirect('https://gumroad.com/l/geocoding-on-rails')
+get '/ios-on-rails' => redirect('https://gumroad.com/l/ios-on-rails')
+get '/ios-on-rails-beta' => redirect('https://gumroad.com/l/ios-on-rails')
+get '/ruby-science' => redirect('https://gumroad.com/l/ruby-science')
