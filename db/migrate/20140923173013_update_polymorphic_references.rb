f319ef57236eb57e6ec1b06fdf42abcb08d83d50
Tute Costa <tutecosta@gmail.com>
2014-09-24 13:09:25 -0400

    Simplify migration
    We don't need to update any reference, because Screencast was a Product, as much
    as now through VideoTutorial.

--- db/migrate/20140923173013_update_polymorphic_references.rb
+++ db/migrate/20140923173013_update_polymorphic_references.rb
@@ -1,24 +1,10 @@
 class UpdatePolymorphicReferences < ActiveRecord::Migration
   def up
-    update_references_from(:classifications, :classifiable_type)
-    update_references_from(:downloads, :purchaseable_type)
-    update_references_from(:licenses, :licenseable_type)
-    update_references_from(:products, :type)
+    say_with_time "Converting Screencasts into VideoTutorials (products.type)" do
+      update "UPDATE products SET type='VideoTutorial' WHERE type='Screencast'"
+    end
   end
 
   def down
-  end
-
-  private
-
-  def update_references_from(table, type_column)
-    say_with_time "Converting Screencasts into VideoTutorials (#{table}.#{type_column})" do
-      # When referenced as Products
-      update <<-SQL
-        UPDATE #{table}
-          SET #{type_column} = 'VideoTutorial'
-          WHERE #{type_column} = 'Screencast'
-      SQL
-    end
   end
 end
