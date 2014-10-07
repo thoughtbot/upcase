23103a668d2086c601827424f24a3c15592317af
Joe Ferris <jferris@thoughtbot.com>
2014-10-07 16:26:46 -0400

    Display completed trails differently
    * Make it clear that the trail is finished
    * Provide positive feedback after finishing a trail
    
    https://trello.com/c/ddCdfkLd/201-show-special-completed-trail-status

--- spec/models/trail_with_progress_spec.rb
+++ spec/models/trail_with_progress_spec.rb
@@ -10,33 +10,42 @@ describe TrailWithProgress do
     expect(trail_with_progress.name).to eq(trail.name)
   end
 
+  describe "#complete?" do
+    context "before receving a review on each exercise" do
+      it "returns false" do
+        trail = create_trail_with_progress(Status::REVIEWED, Status::REVIEWED)
+
+        expect(trail).to be_complete
+      end
+    end
+
+    context "after receving a review on each exercise" do
+      it "returns true" do
+        trail = create_trail_with_progress(Status::REVIEWED, Status::SUBMITTED)
+
+        expect(trail).not_to be_complete
+      end
+    end
+  end
+
   describe "#exercises" do
     context "with no in-progress exercises" do
       it "marks the first unstarted exercise as next up" do
-        exercises = create_list(:exercise, 3)
-        trail = create(:trail, exercises: exercises)
-        user = create(:user)
-        exercises.first.statuses.create!(user: user, state: Status::REVIEWED)
-        trail_with_progress = TrailWithProgress.new(trail, user: user)
+        trail = create_trail_with_progress(Status::REVIEWED, nil, nil)
 
-        result = trail_with_progress.exercises
+        result = trail.exercises
 
         expect(result.first.state).to eq(Status::REVIEWED)
         expect(result.second.state).to eq(Status::NEXT_UP)
         expect(result.third.state).to eq(Status::NOT_STARTED)
-        expect(result.first.title).to eq(exercises.first.title)
       end
     end
 
     context "with an in-progress exercise" do
       it "doesn't mark any exercises as next up" do
-        exercises = create_list(:exercise, 3)
-        trail = create(:trail, exercises: exercises)
-        user = create(:user)
-        exercises.first.statuses.create!(user: user, state: Status::STARTED)
-        trail_with_progress = TrailWithProgress.new(trail, user: user)
+        trail = create_trail_with_progress(Status::STARTED, nil, nil)
 
-        result = trail_with_progress.exercises
+        result = trail.exercises
 
         expect(result.first.state).to eq(Status::STARTED)
         expect(result.second.state).to eq(Status::NOT_STARTED)
@@ -46,18 +55,30 @@ describe TrailWithProgress do
 
     describe "#can_be_accessed?" do
       it "can access if its state is Next Up, or already had access" do
-        exercises = create_list(:exercise, 4)
-        trail = create(:trail, exercises: exercises)
-        user = create(:user)
-        exercises.first.statuses.create!(user: user, state: Status::REVIEWED)
-        trail_with_progress = TrailWithProgress.new(trail, user: user)
+        trail = create_trail_with_progress(Status::REVIEWED, nil, nil, nil)
 
-        result = trail_with_progress.exercises
+        result = trail.exercises
 
         expect(result.first.can_be_accessed?).to be_truthy
         expect(result.second.can_be_accessed?).to be_truthy
         expect(result.third.can_be_accessed?).to be_falsy
         expect(result.fourth.can_be_accessed?).to be_falsy
+      end
+    end
+  end
+
+  def create_trail_with_progress(*states)
+    user = create(:user)
+    exercises =
+      states.map { |state| create_exercise_with_state(state, user: user) }
+    trail = create(:trail, exercises: exercises)
+    TrailWithProgress.new(trail, user: user)
+  end
+
+  def create_exercise_with_state(state, user:)
+    create(:exercise).tap do |exercise|
+      if state.present?
+        exercise.statuses.create!(user: user, state: state)
       end
     end
   end
---
+++ spec/views/dashboards/_trail.html.erb_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+describe "dashboards/_trail.html" do
+  context "with a complete trail" do
+    it "renders as complete" do
+      trail = stub_trail(complete: true)
+
+      render_trail trail
+
+      expect(rendered).to have_content(trail.name)
+      expect(rendered).to have_complete(trail)
+    end
+  end
+
+  context "with an incomplete trail" do
+    it "renders as incomplete" do
+      trail = stub_trail(complete: false)
+
+      render_trail trail
+
+      expect(rendered).to have_content(trail.name)
+      expect(rendered).not_to have_complete(trail)
+    end
+  end
+
+  def stub_trail(complete:)
+    build_stubbed(:trail).tap do |trail|
+      Mocha::Configuration.allow(:stubbing_non_existent_method) do
+        trail.stubs(:complete?).returns(complete)
+      end
+    end
+  end
+
+  def render_trail(trail)
+    view_stubs(:current_user).returns(build_stubbed(:user))
+    render "dashboards/trail", trail: trail
+  end
+
+  def have_complete(trail)
+    have_content(trail.complete_text)
+  end
+end
