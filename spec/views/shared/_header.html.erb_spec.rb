b77e868efc0786e223f744945bb682999eecf646
ldamon <lydia@thoughtbot.com>
2014-10-27 12:52:26 -0400

    Add Upcase header with new logo, colors and layout

--- spec/views/shared/_header.html.erb_spec.rb
+++ spec/views/shared/_header.html.erb_spec.rb
@@ -7,13 +7,11 @@ describe "shared/_header.html.erb" do
 
   context "when user is the subscription owner" do
     it "shows an annual upsell link" do
-      view_stubs(masquerading?: false)
-      view_stubs(signed_in?: true)
-      view_stubs(current_user_has_active_subscription?: true)
-      view_stubs(current_user_is_subscription_owner?: true)
-      view_stubs(current_user_has_monthly_subscription?: true)
-
-      render
+      render(
+        current_user_has_active_subscription: true,
+        current_user_is_subscription_owner: true,
+        signed_in: true
+      )
 
       expect(rendered).to have_content(call_to_action_label)
     end
@@ -21,12 +19,11 @@ describe "shared/_header.html.erb" do
 
   context "when user is not the subscription owner" do
     it "does not show an annual upsell link" do
-      view_stubs(masquerading?: false)
-      view_stubs(signed_in?: true)
-      view_stubs(current_user_has_active_subscription?: true)
-      view_stubs(current_user_is_subscription_owner?: false)
-
-      render
+      render(
+        current_user_has_active_subscription: true,
+        current_user_is_subscription_owner: false,
+        signed_in: true
+      )
 
       expect(rendered).not_to have_content(call_to_action_label)
     end
@@ -40,9 +37,68 @@ describe "shared/_header.html.erb" do
       view_stubs(current_user_is_subscription_owner?: true)
       view_stubs(current_user_has_monthly_subscription?: false)
 
-      render
+      render(
+        current_user_has_active_subscription: true,
+        current_user_has_monthly_subscription: false,
+        current_user_is_subscription_owner: true,
+        signed_in: true
+      )
 
       expect(rendered).not_to have_content(call_to_action_label)
     end
+  end
+
+  context "when the user isn't signed in" do
+    it "shows a plans and pricing link" do
+      render(signed_in: false)
+
+      expect(rendered).to have_content(I18n.t("subscriptions.join_cta"))
+    end
+  end
+
+  context "when the user isn't subscribed" do
+    it "shows a membership link" do
+      render(
+        signed_in: true,
+        current_user_has_active_subscription: false
+      )
+
+      expect(rendered).to have_css("a[href='#{subscribe_path}']")
+    end
+  end
+
+  context "when the user has a subscription" do
+    it "does not show a membership link" do
+      render(
+        signed_in: true,
+        current_user_has_active_subscription: true
+      )
+
+      expect(rendered).not_to have_css("a[href='#{subscribe_path}']")
+    end
+  end
+
+  def render(
+    current_user_has_active_subscription: true,
+    current_user_has_monthly_subscription: true,
+    current_user_is_subscription_owner: true,
+    masquerading: false,
+    signed_in: true
+  )
+    view_stubs(
+      current_user_has_active_subscription?:
+        current_user_has_active_subscription
+    )
+    view_stubs(
+      current_user_has_monthly_subscription?:
+        current_user_has_monthly_subscription
+    )
+    view_stubs(
+      current_user_is_subscription_owner?:
+        current_user_is_subscription_owner
+    )
+    view_stubs(masquerading?: masquerading)
+    view_stubs(signed_in?: signed_in)
+    super()
   end
 end
