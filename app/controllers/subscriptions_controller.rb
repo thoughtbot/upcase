8f40115994c52ba822ce44be6b89d149e263e5ee
Tute Costa <tutecosta@gmail.com>
2014-11-17 15:53:32 -0500

    Remove repeated footer form subscriptions/edit

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,18 +4,19 @@ class SubscriptionsController < ApplicationController
   def new
     @catalog = Catalog.new
 
-    render :layout => 'empty-body'
+    render layout: "empty-body"
   end
 
   def edit
     @catalog = Catalog.new
 
-    render :layout => 'header-only'
+    render layout: "header-only"
   end
 
   def update
     plan = Plan.find_by_sku!(params[:plan_id])
     current_user.subscription.change_plan(plan)
-    redirect_to my_account_path, notice: I18n.t('subscriptions.flashes.change.success')
+    redirect_to my_account_path,
+                notice: I18n.t("subscriptions.flashes.change.success")
   end
 end
