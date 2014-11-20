53489617caa87fc2aa999bc9587c1d1f4e241de5
Joe Ferris <jferris@thoughtbot.com>
2014-11-19 19:27:40 -0500

    Hound

--- app/helpers/mentor_helper.rb
+++ app/helpers/mentor_helper.rb
@@ -1,13 +1,13 @@
 module MentorHelper
   def mentor_image(mentor)
-    image_tag gravatar_url(mentor.email, size: '300')
+    image_tag gravatar_url(mentor.email, size: "300")
   end
 
   def mentor_contact_link(mentor)
     mail_to(
       mentor.email,
       I18n.t(
-        'shared.header.contact_your_mentor',
+        "shared.header.contact_your_mentor",
         mentor_name: mentor.first_name
       )
     )
