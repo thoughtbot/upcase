3e94ccfa5622f974b9f8461743532afbe9c72b4a
Tute Costa <tutecosta@gmail.com>
2014-12-16 13:46:15 -0500

    Redirect TWI subscribers to the show page
    https://trello.com/c/5jn4xePM/496-make-accessible-content-easier-to-find-for-weekly-iteration-subscribers

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,10 +3,24 @@ class HomesController < ApplicationController
 
   def show
     if signed_in?
-      redirect_to practice_path
+      redirect_user_to_dashboard
     else
       @community_size = User.subscriber_count
       render template: "landing_pages/watch-one-do-one-teach-one"
     end
+  end
+
+  private
+
+  def redirect_user_to_dashboard
+    if show_exercises?
+      redirect_to practice_path
+    else
+      redirect_to Show.the_weekly_iteration
+    end
+  end
+
+  def show_exercises?
+    current_user.subscription.nil? || current_user.has_access_to?(:exercises)
   end
 end
--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -15,4 +15,25 @@ describe HomesController do
 
     expect(response).to redirect_to practice_path
   end
+
+  it "redirects to products if subscriber has access to exercises" do
+    plan = build_stubbed(:plan, includes_exercises: true)
+    subscriber = create(:user, :with_subscription, plan: plan)
+    sign_in_as subscriber
+
+    get :show
+
+    expect(response).to redirect_to practice_path
+  end
+
+  it "redirects to The Weekly Iteration if subscriber has no exercise access" do
+    create(:show, name: Show::THE_WEEKLY_ITERATION)
+    plan = build_stubbed(:plan, includes_exercises: false)
+    subscriber = create(:user, :with_subscription, plan: plan)
+    sign_in_as subscriber
+
+    get :show
+
+    expect(response).to redirect_to Show.the_weekly_iteration
+  end
 end
