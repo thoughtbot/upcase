e87d967b347f2af9fc4f7d05191c626b9a39e640
Tute Costa <tutecosta@gmail.com>
2014-12-18 14:02:03 -0500

    Sets max-age Cache-Control HTTP header

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,14 +6,22 @@ class TopicsController < ApplicationController
       format.css { render content_type: "text/css" }
     end
 
+    set_cache_headers
+  end
+
+  def show
+    @topic = Topic.find_by_slug!(params[:id])
+  end
+
+  private
+
+  def set_cache_headers
+    expires_in Rails.configuration.static_cache_control, public: true
+
     fresh_when(
       etag: @topics.maximum(:updated_at),
       last_modified: @topics.maximum(:updated_at),
       public: true
     )
-  end
-
-  def show
-    @topic = Topic.find_by_slug!(params[:id])
   end
 end
