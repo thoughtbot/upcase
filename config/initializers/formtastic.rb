63fa25a4da87a8ea6d8fc7c8a508b8b6a16b57d8
Arun Agrawal <arunagw@gmail.com>
2014-12-30 10:59:18 +0100

    Update formtastic gem

--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -1,1 +1,3 @@
+Formtastic::FormBuilder.action_class_finder = Formtastic::ActionClassFinder
+Formtastic::FormBuilder.input_class_finder = Formtastic::InputClassFinder
 Formtastic::FormBuilder.all_fields_required_by_default = false
