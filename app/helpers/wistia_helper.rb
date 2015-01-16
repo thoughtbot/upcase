thoughtbot/upcase

commit 92a8c3292c0cb417fd199531e897a7400bbba85e

Track user video viewing
* Track video event in analytics backend
* Create an associated Status object for video
* Mark the status as in-progress after user starts the video
* Mark the status as complete after user passes the 80% mark
* Generalize `Api::V1::StatusesController`

https://trello.com/c/vUa3NXlB

app/assets/stylesheets/_video.scss
@@ -2,17 +2,7 @@
   height: 0;
   margin: 0 0 2em 0;
   overflow: hidden;
-  padding-bottom: 56.25%;
-  padding-top: 0px;
   position: relative;
-
-  iframe {
-    height: 100%;
-    left: 0;
-    position: absolute;
-    top: 0;
-    width: 100%;
-  }
 }
 
 .wistia-holder {

app/views/videos/show_subscribed.html.erb
@@ -1,7 +1,7 @@
 <% content_for :page_title, @video.title.html_safe %>
 
 <% content_for :javascript do %>
-  <script src="//fast.wistia.net/static/iframe-api-v1.js"></script>
+  <script src="//fast.wistia.com/assets/external/E-v1.js"></script>
 <% end %>
 
 <% if @watchable.collection? %>

app/assets/javascripts/wistia_helper.js.coffee
@@ -2,21 +2,31 @@ class @WistiaHelper
   embedVideos: ->
     videos = $('.wistia-video[data-wistia-id]')
     for video in videos
-      @_embedIframe(video)
+      @_embedVideo(video)
 
   embedThumbnails: ->
     thumbnails = $('.wistia-thumbnail[data-wistia-id]')
     for thumbnail in thumbnails
       @_insertThumbnailUrl(thumbnail)
 
-  _embedIframe: (video) ->
-    $video = $(video)
-    width = $video.data('width')
-    height = $video.data('height')
-    params = "?videoWidth=#{width}&videoHeight=#{height}&controlsVisibleOnLoad=true"
-    $.getJSON(@_encodedUrl($video, params), (data) ->
-      $video.html(data.html)
-    )
+  _embedVideo: (video) ->
+    hashedId = $(video).data('wistia-id')
+    $(video).prop("id", "wistia_#{hashedId}")
+    window.wistiaEmbed = Wistia.embed hashedId,
+      controlsVisibleOnLoad: true
+      videoFoam: true
+    wistiaEmbed.bind "play", ->
+      unless wistiaEmbed.started
+        $.post "/api/v1/videos/#{hashedId}/status",
+          state: "In Progress"
+        wistiaEmbed.started = true
+    wistiaEmbed.bind "secondchange", (second) ->
+      wistiaEmbed.watchedThreshold ||= Math.floor(wistiaEmbed.duration() * 0.8)
+
+      if second > wistiaEmbed.watchedThreshold && !wistiaEmbed.watched
+        $.post "/api/v1/videos/#{hashedId}/status",
+          state: "Complete"
+        wistiaEmbed.watched = true
 
   _insertThumbnailUrl: (thumbnail) ->
     $thumbnail = $(thumbnail)
@@ -33,7 +43,7 @@ class @WistiaHelper
   _encodedUrl: ($elem, params) ->
     hashedId = $elem.data('wistia-id')
     baseUrl = WistiaHelper.host + "/oembed.json?url="
-    mediaUrl = "http://thoughtbotlearn.wistia.com/medias/#{hashedId}"
+    mediaUrl = WistiaHelper.host + "/medias/#{hashedId}"
     encodedUrl = encodeURIComponent(mediaUrl + params)
     baseUrl + encodedUrl
 

app/controllers/api/v1/completions_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CompletionsController < ApiController
-  before_action :doorkeeper_authorize!, if: lambda { !signed_in? }
+  before_action :doorkeeper_authorize!, unless: :signed_in?
 
   def index
     respond_with current_resource_owner.completions.only_trail_object_ids
@@ -28,9 +28,5 @@ class Api::V1::CompletionsController < ApiController
 
   def lookup_completion(trail_object_id)
     current_resource_owner.completions.find_by_trail_object_id!(trail_object_id)
-  end
-
-  def current_resource_owner
-    current_user || resource_owner
   end
 end

app/controllers/api/v1/statuses_controller.rb
@@ -1,15 +1,31 @@
 class Api::V1::StatusesController < ApiController
-  before_action :doorkeeper_authorize!
+  before_action :doorkeeper_authorize!, unless: :signed_in?
 
   def create
-    exercise.statuses.create!(user: resource_owner, state: params[:state])
-    exercise.update_trails_state_for(resource_owner)
+    completable.statuses.create!(
+      user: current_resource_owner,
+      state: params[:state]
+    )
+    completable.update_trails_state_for(current_resource_owner)
+    analytics.track_status_created(completable, params[:state])
     render nothing: true
   end
 
   private
 
-  def exercise
-    @exercise ||= Exercise.find_by!(uuid: params[:exercise_uuid])
+  def completable
+    @completable ||= find_completable
+  end
+
+  def find_completable
+    if params[:exercise_uuid]
+      Exercise.find_by_uuid!(params[:exercise_uuid])
+    elsif params[:video_wistia_id]
+      Video.find_by_wistia_id!(params[:video_wistia_id])
+    end
+  end
+
+  def analytics
+    Analytics.new(current_user)
   end
 end

app/controllers/api_controller.rb
@@ -8,4 +8,8 @@ class ApiController < ApplicationController
   def resource_owner
     User.find(doorkeeper_token.resource_owner_id)
   end
+
+  def current_resource_owner
+    current_user || resource_owner
+  end
 end

app/models/video.rb
@@ -4,6 +4,7 @@ class Video < ActiveRecord::Base
   belongs_to :watchable, polymorphic: true
   has_many :classifications, as: :classifiable
   has_many :topics, through: :classifications
+  has_many :statuses, as: :completeable, dependent: :destroy
 
   validates :published_on, presence: true
   validates :slug, presence: true, uniqueness: true
@@ -51,5 +52,10 @@ class Video < ActiveRecord::Base
 
   def to_param
     slug
+  end
+
+  def update_trails_state_for(_user)
+    # Waiting on story for adding Videos as steps of a trail, does
+    # nothing for now.
   end
 end

app/services/analytics.rb
@@ -20,6 +20,16 @@ class Analytics
     track(event: "Logged into Forum", properties: {})
   end
 
+  def track_status_created(completable, state)
+    if completable.is_a? Video
+      if state == Status::IN_PROGRESS
+        track(event: "Started video", properties: { video_id: completable.id })
+      elsif state == Status::COMPLETE
+        track(event: "Finished video", properties: { video_id: completable.id })
+      end
+    end
+  end
+
   private
 
   attr_reader :user

commit cbff99e07e5563839a92880505669ded761e298d

Fix Wistia embed for previews
These previews were broken with the tracking introduced in 92a8c329.

The following errors were introduced:

* The Wistia JavaScript was only included on one page
* The Wistia responsive design was fighting our CSS

The commit:

* Introduces the Wistia JavaScript on all pages with embeds
* Removes our CSS hacks to depend on Wistia's "video foam"

https://trello.com/c/cz3T9R6u/559

app/assets/stylesheets/_video.scss
@@ -1,25 +1,11 @@
-.videowrapper, .mapwrapper {
-  height: 0;
-  margin: 0 0 2em 0;
-  overflow: hidden;
-  position: relative;
-}
-
-.wistia-holder {
-  width: auto;
-  background-color: #333;
-  border: 1px solid #111;
-  margin: 0 auto;
-  padding: 40px 0 40px 0;
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-
-  .wistia_embed {
-    margin: 0 auto;
-    max-height: 392px;
-  }
+/*
+ * Wistia requires a parent element that does NOT calculate its with based on
+ * the width of its children.
+ *
+ * http://wistia.com/doc/video-foam
+ */
+.wistia-wrapper {
+  width: 100%;
 }
 
 .assets-wrapper {

app/helpers/wistia_helper.rb
@@ -3,7 +3,7 @@ module WistiaHelper
     content_tag(
       'p',
       nil,
-      class: "videowrapper wistia-video",
+      class: "wistia-video",
       data: {
         wistia_id: clip.wistia_id,
         width: "653",

app/views/clips/_clip.html.erb
@@ -1,4 +1,6 @@
-<%= large_wistia_iframe_for(clip) %>
+<div class="wistia-wrapper">
+  <%= large_wistia_iframe_for(clip) %>
+</div>
 <section class="assets-wrapper">
   <div class="assets">
     <%= render 'videos/download_link', download_type_key: "OriginalFile", download_type: "original", size_display: "Original (720p)", clip: clip %>
@@ -6,3 +8,7 @@
     <%= render 'videos/download_link', download_type_key: "HdMp4VideoFile", download_type: "hd_mp4", size_display: "HD MP4", clip: clip %>
   </div>
 </section>
+
+<% content_for :javascript do %>
+  <script src="//fast.wistia.com/assets/external/E-v1.js"></script>
+<% end %>

app/views/shows/show.html.erb
@@ -7,9 +7,7 @@
 <div class="text-box-wrapper">
   <%= render "watchables/preview" %>
   <div class="text-box">
-    <p class="videowrapper">
-      <iframe src="//fast.wistia.net/embed/iframe/ol6e0miehm?controlsVisibleOnLoad=true&version=v1&videoHeight=415&videoWidth=738&volumeControl=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" width="653" height="367"></iframe>
-    </p>
+    <%= render Clip.new("ol6e0miehm") %>
 
     <section id="videos">
       <% @show.published_videos.recently_published_first.each do |video| %>

app/views/videos/show_subscribed.html.erb
@@ -1,9 +1,5 @@
 <% content_for :page_title, @video.title.html_safe %>
 
-<% content_for :javascript do %>
-  <script src="//fast.wistia.com/assets/external/E-v1.js"></script>
-<% end %>
-
 <% if @watchable.collection? %>
   <% content_for :additional_header_links do %>
     <li class="all-videos"><%= link_to 'All Videos', @video.watchable %></li>

spec/views/videos/show.html.erb_spec.rb
@@ -17,7 +17,7 @@ describe "videos/show" do
     render template: "videos/show"
 
     expect(rendered).
-      to have_css("p.videowrapper[data-wistia-id='#{wistia_id}']")
+      to have_css("p[data-wistia-id='#{wistia_id}']")
   end
 
   it "shows a thumbnail when there is no preview" do