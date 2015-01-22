ee9ece7e7647b19e137ababd590047f965ef48dd
Eric Collins <eric@thoughtbot.com>
2015-01-22 18:12:19 -0500

    Rename remember token to be upcase specific
    * Fixed url for signed in users accessing oauth
    * Change comes from a new feature in 1.8.0
    * Implements redirect_signed_in_users method
    * https://github.com/thoughtbot/clearance/commit/4f2c14c8fc303c0cba993e35d69e614aa7468d9f

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,5 @@
 Clearance.configure do |config|
   config.mailer_sender = "Upcase <#{ENV["SUPPORT_EMAIL"]}>"
   config.password_strategy = Clearance::PasswordStrategies::SHA1
+  config.cookie_name = "upcase_remember_token"
 end
--- spec/features/user_attempts_to_add_oauth_application_spec.rb
+++ spec/features/user_attempts_to_add_oauth_application_spec.rb
@@ -4,7 +4,7 @@ feature 'User attempts to add OAuth application' do
   scenario 'as non-admin' do
     user = create(:user)
     visit oauth_applications_path(as: user)
-    expect(page.current_url).to eq sign_in_url
+    expect(page.current_url).to eq practice_url
   end
 
   scenario 'when not authenticated' do
