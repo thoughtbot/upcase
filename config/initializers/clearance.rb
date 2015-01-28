23ffe1dd58e8230e268c7d7cd62aaae9fc7c58fb
Eric Collins <eric@thoughtbot.com>
2015-01-28 16:45:52 -0500

    Set cookie info through environment variables
    * Set cookie name default to upcase_remember_token
    * Do not set default cookie domain, it is not needed

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,7 @@
 Clearance.configure do |config|
   config.mailer_sender = "Upcase <#{ENV["SUPPORT_EMAIL"]}>"
   config.password_strategy = Clearance::PasswordStrategies::SHA1
-  config.cookie_name = "upcase_remember_token"
+  config.cookie_name = ENV["CLEARANCE_COOKIE_NAME"]
+  config.cookie_domain = ENV["CLEARANCE_COOKIE_DOMAIN"]
   config.routes = false
 end
