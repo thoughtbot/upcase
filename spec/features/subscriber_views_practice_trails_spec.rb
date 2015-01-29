e2c8a779ff265ae31d3acedbf4a90d18f125c83a
Elle Meredith <ellemeredith@gmail.com>
2015-01-29 13:32:27 -0500

    practice: trail partials

--- spec/features/subscriber_views_practice_trails_spec.rb
+++ spec/features/subscriber_views_practice_trails_spec.rb
@@ -2,14 +2,22 @@ require "rails_helper"
 
 feature "Subscriber views completed trails" do
   scenario "completed and incompleted trails are separated" do
+
     completed_trail = create(:trail, :published, :completed)
     incomplete_trail = create(:trail, :published)
     user = completed_trail.users.last
     sign_in_as user
 
     visit practice_path
-    #save_and_open_page
-    expect(page).to have_content(completed_trail.name)
-    expect(page).to have_content(incomplete_trail.name)
+
+    within '.completed-trails' do
+      expect(page).to have_content(completed_trail.name)
+      expect(page).to have_no_content(incomplete_trail.name)
+    end
+
+    within '.active-trails' do
+      expect(page).to have_no_content(completed_trail.name)
+      expect(page).to have_content(incomplete_trail.name)
+    end
   end
 end
