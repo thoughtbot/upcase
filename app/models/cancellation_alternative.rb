40dfb2e999d8c104bcfc70c15dba9ab90a4942cf
Ben Orenstein <ben.orenstein@gmail.com>
2015-04-10 11:55:52 -0400

    Extract methods to clean up calcuation logic

--- app/models/cancellation_alternative.rb
+++ app/models/cancellation_alternative.rb
@@ -9,11 +9,23 @@ class CancellationAlternative
   end
 
   def discount_percentage_vs_current_plan_annualized
-    ((1 - (@discounted_plan.price_in_dollars / (@current_plan.price_in_dollars * 12.0))) * 100).
-      round(0)
+    discount_percentage_for(
+      new_price: discount_plan_price,
+      current_price: current_annual_price_in_dollars
+    )
   end
 
   def discount_plan_price
     @discounted_plan.price_in_dollars
+  end
+
+  private
+
+  def discount_percentage_for(new_price:, current_price:)
+    ((1 - (new_price / current_price)) * 100).round(0)
+  end
+
+  def current_annual_price_in_dollars
+    @current_plan.price_in_dollars * 12.0
   end
 end
