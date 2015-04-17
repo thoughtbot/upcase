88e92ce487b7951f7444e039259b163791c55ee6
Chris Toomey <chris@ctoomey.com>
2015-04-17 10:53:40 -0400

    Add environment banner in staging and dev
    The environment banner displays the environment name as well as the current
    branch. This is useful to visually distinguish environments (including
    production by the lack of a banner), as well as quickly answering the question
    of what is currently deployed.
    
    Unfortunately Heroku deploys only the code, not the git repo, so branch and
    commit sha must be pushed from the deploying environment which is done via the
    bin/deploy script.

---
+++ app/helpers/environment_banner_helper.rb
@@ -0,0 +1,22 @@
+module EnvironmentBannerHelper
+  def current_branch
+    if git_available?
+      `git rev-parse --abbrev-ref HEAD`.chomp
+    else
+      ENV.fetch("CURRENT_BRANCH", "--branch-not-found--")
+    end
+  end
+
+  def current_sha
+    if git_available?
+      `git log -1 --abbrev-commit --oneline | cut -d ' ' -f 1`
+    else
+      ENV.fetch("CURRENT_SHA", "--sha-not-found--")
+    end
+  end
+
+  def git_available?
+    to_dev_null = "> /dev/null 2>&1"
+    system("which git #{to_dev_null} && git rev-parse --git-dir #{to_dev_null}")
+  end
+end
