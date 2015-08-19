113647318fef683c31d1874c810ace003be94ebf
Magnus Gyllenswärd <magnus@thoughtbot.com>
2015-08-19 11:15:09 +0200

    Create new checkout page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,14 @@ class PagesController < HighVoltage::PagesController
   def layout_for_page
     if params[:id] == "new-product"
       "header-only"
-    elsif params[:id] == "landing"
+    elsif landing_page?
       "landing_pages"
     else
       "application"
     end
+  end
+
+  def landing_page?
+    %w{ landing checkout }.include?(params[:id])
   end
 end
