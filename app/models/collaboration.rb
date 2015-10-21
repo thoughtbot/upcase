80c5a49a23031e042291fe2e7fe05c9cc948aa20
Ben Orenstein <ben.orenstein@gmail.com>
2015-10-21 14:46:17 -0400

    Validate presence of ids on Collaboration

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -1,4 +1,7 @@
 class Collaboration < ActiveRecord::Base
   belongs_to :repository
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :repository_id, presence: true
 end
--- spec/models/collaboration_spec.rb
+++ spec/models/collaboration_spec.rb
@@ -3,4 +3,7 @@ require "rails_helper"
 describe Collaboration do
   it { should belong_to(:repository) }
   it { should belong_to(:user) }
+
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:repository_id) }
 end
