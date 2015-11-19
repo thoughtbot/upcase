c4219ce63b72bf5651f568baeef0739fd23712d0
Chris Toomey <chris@ctoomey.com>
2015-11-19 13:17:54 -0500

    Add upgrade link to footer for non-subscribers
    This link certainly can't hurt from a conversion standpoint, but the primary
    purpose is to provide something on page for analtyics tools to determine the
    current user state from the three possible:
    
    - visitor (signed_out)
    - sampler (signed in, no active subscription)
    - subscriber (signed in w/ active subscription)

--- spec/views/shared/_footer.html.erb_spec.rb
+++ spec/views/shared/_footer.html.erb_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe "shared/_footer.html.erb" do
   context "when not signed in" do
     it "does not show the repositories link" do
-      view_stub_with_return(signed_in?: false)
+      stub_signed_in(false)
 
       render
 
@@ -11,7 +11,7 @@ describe "shared/_footer.html.erb" do
     end
 
     it "does show the contact us link" do
-      view_stub_with_return(signed_in?: false)
+      stub_signed_in(false)
 
       render
 
@@ -19,7 +19,7 @@ describe "shared/_footer.html.erb" do
     end
 
     it "does show a sign in link" do
-      view_stub_with_return(signed_in?: false)
+      stub_signed_in(false)
 
       render
 
@@ -30,8 +30,8 @@ describe "shared/_footer.html.erb" do
 
   context "when user is signed in" do
     it "does show the repositories link" do
-      view_stub_with_return(signed_in?: true)
-      view_stub_with_return(current_user: create(:user))
+      stub_signed_in(true)
+      stub_current_user_with create(:user)
 
       render
 
@@ -39,8 +39,8 @@ describe "shared/_footer.html.erb" do
     end
 
     it "does not show the contact us link" do
-      view_stub_with_return(signed_in?: true)
-      view_stub_with_return(current_user: create(:user))
+      stub_signed_in(true)
+      stub_current_user_with create(:user)
 
       render
 
@@ -48,13 +48,76 @@ describe "shared/_footer.html.erb" do
     end
 
     it "does show a sign out link" do
-      view_stub_with_return(signed_in?: true)
-      view_stub_with_return(current_user: create(:user))
+      stub_signed_in(true)
+      stub_current_user_with create(:user)
 
       render
 
       expect(rendered).to have_content("Sign out")
       expect(rendered).not_to have_content("Sign in")
     end
+
+    context "when the user is not an admin" do
+      it "does not render a link to the admin" do
+        stub_signed_in(true)
+        stub_current_user_with build_stubbed(:user)
+
+        render
+
+        expect(rendered).not_to have_link("Admin", href: admin_path)
+      end
+    end
+
+    context "when the user is an admin" do
+      it "renders a link to the admin" do
+        stub_signed_in(true)
+        stub_current_user_with build_stubbed(:admin)
+
+        render
+
+        expect(rendered).to have_link("Admin", href: admin_path)
+      end
+    end
+
+    context "when the user does not have an active subscription" do
+      it "includes an 'Upgrade' link" do
+        stub_signed_in(true)
+        stub_current_user_with build_stubbed(:user)
+
+        render
+
+        expect(rendered).to have_upgrade_link
+      end
+    end
+
+    context "when the user has an active subscription" do
+      it "does not include an 'Upgrade' link" do
+        stub_signed_in(true)
+        subscriber = build_user_with_Active_subscription
+        stub_current_user_with subscriber
+
+        render
+
+        expect(rendered).not_to have_upgrade_link
+      end
+    end
+  end
+
+  def have_upgrade_link
+    have_link("Upgrade", href: join_path)
+  end
+
+  def build_user_with_Active_subscription
+    subscriber = build_stubbed(:subscriber).tap do |subscriber|
+      allow(subscriber).to receive(:has_active_subscription?).and_return(true)
+    end
+  end
+
+  def stub_signed_in(result)
+    view_stub_with_return(signed_in?: result)
+  end
+
+  def stub_current_user_with(user)
+    view_stub_with_return(current_user: user)
   end
 end
