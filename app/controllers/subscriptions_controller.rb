bc01657d935a3df515267b5cd91746255d504ea6
Chris Toomey <chris@ctoomey.com>
2015-12-05 10:58:48 -0500

    Hardcode V5 version of the home page updates
    Based on the results of a VWO test running for the past few weeks, we've seen
    significant improvement of conversions with this variant of the home page. This
    change locks in those wins so we can disable VWO.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,6 +2,7 @@ class SubscriptionsController < ApplicationController
   before_filter :must_be_subscription_owner, only: [:edit, :update]
 
   def new
+    @topics = define_topics
   end
 
   def edit
@@ -12,5 +13,24 @@ class SubscriptionsController < ApplicationController
     current_user.subscription.change_plan(sku: params[:plan_id])
     redirect_to my_account_path,
                 notice: I18n.t("subscriptions.flashes.change.success")
+  end
+
+  private
+
+  LandingTopic = Struct.new(:name, :slug, :image, :target)
+
+  def define_topics
+    [
+      ["Clean Code", "clean-code", "clean-code", topic_path("clean-code")],
+      ["Git", "git", "workflow", video_path("git-workflow")],
+      ["Ruby on Rails", "rails", "rails", topic_path("rails")],
+      ["Testing", "testing", "testing", topic_path("testing")],
+      ["iOS", "ios", "ios", topic_path("ios")],
+      ["Haskell", "haskell", "haskell", topic_path("haskell")],
+      ["JavaScript", "javascript", "javascript", topic_path("javascript")],
+      ["Vim", "vim", "vim", topic_path("vim")],
+      ["tmux", "tmux", "workflow", trail_path("tmux")],
+      ["Design", "design", "design", topic_path("design")],
+    ].map { |topic_details| LandingTopic.new(*topic_details) }
   end
 end
