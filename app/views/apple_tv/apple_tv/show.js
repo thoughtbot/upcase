7068117a9290ed46506d1861eaf04b1b7a26260f
Jason Draper <jason@drapergeek.com>
2015-12-10 12:43:54 -0500

    Handle URLS

--- app/views/apple_tv/apple_tv/show.js
+++ app/views/apple_tv/apple_tv/show.js
@@ -4,19 +4,32 @@ function getDocument(url) {
     templateXHR.addEventListener("load", function() {pushDoc(templateXHR.responseXML);}, false);
     templateXHR.open("GET", url, true);
     templateXHR.send();
+
     return templateXHR;
 }
 
 function pushDoc(document) {
+    document.addEventListener("select", select);
     navigationDocument.pushDocument(document);
 }
 
 App.onLaunch = function(options) {
     console.log("HI");
     var templateURL = 'http://localhost:5000/apple_tv/weekly_iterations.xml';
+
     getDocument(templateURL);
 }
 
 App.onExit = function() {
     console.log('App finished');
+}
+
+function select(event) {
+  var self = this,
+  ele = event.target,
+  templateURL = ele.getAttribute("template");
+
+  if (templateURL) {
+    getDocument(templateURL);
+  }
 }
