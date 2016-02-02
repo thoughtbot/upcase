d04cce8c2b584855404d07b7d2a643027cd027fa
Matthew Sumner <matt.m.sumner@gmail.com>
2016-02-02 14:44:55 -0500

    Add teams link into landing header
    Why:
    
    * We'd like for users to know there is a team plan if they have a team
      they'd like to sign up.
    * There should be a clear flow for signing up for a team subscription.
    
    This PR:
    
    * Adds a teams link to the landing layout.

--- spec/features/visitor_purchases_team_subscription_spec.rb
+++ spec/features/visitor_purchases_team_subscription_spec.rb
@@ -2,7 +2,9 @@ require "rails_helper"
 
 feature 'Visitor can purchase a subscription for their team' do
   scenario 'successful purchase' do
-    visit_team_plan_checkout_page
+    create_team_plan
+    navigate_to_team_sign_up
+
     fill_out_account_creation_form email: 'user@fabtabulous.com'
     fill_out_credit_card_form_with_valid_credit_card
 
@@ -13,8 +15,14 @@ feature 'Visitor can purchase a subscription for their team' do
     expect_to_see_team_name 'Fabtabulous'
   end
 
-  def team_plan
-    Plan.team.first
+  def create_team_plan
+    create(:plan, :team, :featured)
+  end
+
+  def navigate_to_team_sign_up
+    visit root_path
+    click_link I18n.t("shared.header.teams")
+    click_link I18n.t("teams.join_cta")
   end
 
   def expect_to_see_team_name(team_name)
