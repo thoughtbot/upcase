6f39274c19472fd503eaeb03afddbf91624ef877
Matt Jankowski <mjankowski@thoughtbot.com>
2017-03-17 15:20:49 -0400

    Rename stripe env vars to be more accurate

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,3 @@
-Stripe.api_key = ENV['STRIPE_API_KEY'] || 'api_key'
-STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY'] || 'public_key'
+Stripe.api_key = ENV["STRIPE_API_SECRET_KEY"] || "api_secret_key"
+STRIPE_API_PUBLISHABLE_KEY = ENV["STRIPE_API_PUBLISHABLE_KEY"] || "api_public_key"
 Stripe.api_version = "2014-09-08"
