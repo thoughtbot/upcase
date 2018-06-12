a4c19a118c379933f7e84257998d0ec0742272aa
Matt Jankowski <mjankowski@thoughtbot.com>
2018-06-12 15:26:42 -0400

    Run `rails app:update` for rails five changes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Upcase::Application.config.session_store(:cookie_store,
+Rails.application.config.session_store(:cookie_store,
                                          key: "_workshops_session")
