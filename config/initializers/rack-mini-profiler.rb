b9786db64c0b726d576bcc67bd87c49db14a91e8
Matt Jankowski <mjankowski@thoughtbot.com>
2018-06-28 12:01:43 -0400

    Remove no longer needed custom configuration for rack mini profiler

--- config/initializers/rack-mini-profiler.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env.development?
-  require "rack-mini-profiler"
-
-  Rack::MiniProfilerRails.initialize!(Rails.application)
-
-  Rails.application.middleware.delete(Rack::MiniProfiler)
-  Rails.application.middleware.insert_after(Rack::Deflater, Rack::MiniProfiler)
-end
