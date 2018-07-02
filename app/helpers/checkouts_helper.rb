44cffaea46b7992dc46a46ea7b51d2726995647d
Matt Jankowski <mjankowski@thoughtbot.com>
2018-07-02 09:10:38 -0400

    Use query attribute for stripe coupon id

--- app/helpers/checkouts_helper.rb
+++ app/helpers/checkouts_helper.rb
@@ -14,10 +14,10 @@ module CheckoutsHelper
   end
 
   def formatted_subscription_price(checkout)
-    if checkout.stripe_coupon_id.blank?
-      number_to_currency(checkout.price, precision: 0)
-    else
+    if checkout.stripe_coupon_id?
       formatted_subscription_price_with_coupon(checkout)
+    else
+      number_to_currency(checkout.price, precision: 0)
     end
   end
 
