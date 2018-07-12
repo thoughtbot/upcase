20e0a58a301c1f966ee25d4a67f30cc571c20c32
Matt Jankowski <mjankowski@thoughtbot.com>
2018-07-12 13:54:07 -0400

    Add coverage for sitemap

---
+++ spec/requests/sitemap_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+describe "The sitemap" do
+  it "responds with an XML sitemap" do
+    create(:show, name: Show::THE_WEEKLY_ITERATION)
+
+    get "/upcase/sitemap.xml", params: { format: :xml }
+
+    expect(response).to be_successful
+    expect(response.content_type).to eq("application/xml")
+  end
+end
