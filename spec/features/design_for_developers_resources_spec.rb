ecb1ccd237b44c1ec82a33b733246d3c16af1cca
Matt Jankowski <mjankowski@thoughtbot.com>
2018-07-13 10:29:24 -0400

    Add coverage for d4d#show action

--- spec/features/design_for_developers_resources_spec.rb
+++ spec/features/design_for_developers_resources_spec.rb
@@ -7,6 +7,12 @@ feature 'Design For Developers Resources' do
     expect(page).to have_css("a[href*='#{amazon_product_id}']")
   end
 
+  scenario 'View a specific resource' do
+    visit "/upcase/design-for-developers-resources/brief"
+
+    expect(page).to have_content('your design brief')
+  end
+
   def amazon_product_id
     # https://www.amazon.com/Visual-Display-Quantitative-Information-2nd/dp/0961392142
     '0961392142'
---
+++ spec/requests/design_for_developers_resource_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+describe "Design For Developers Resources" do
+  it "handles a missing resource" do
+    expect { get_missing_resource }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  def get_missing_resource
+    get "/upcase/design-for-developers-resources/missing-resource"
+  end
+end
