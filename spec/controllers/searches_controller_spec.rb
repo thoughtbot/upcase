92e4e359571d8fc0fd3fa2ef84c8af37ad00eb02
Chris Toomey <chris@ctoomey.com>
2018-09-21 16:07:45 -0400

    Allow signed out users to access the /search page
    Note: this change ran into a small issue where the search page was
    attempting to track the Search term, and was relying on having a signed
    in user with an ID to track against. To solve this I implemented the
    `id` method on `Guest` to return the string `"guest"` which I think both
    solves the problem, and could potentially be useful from a tracking
    perspective.

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -2,8 +2,6 @@ require "rails_helper"
 
 describe SearchesController do
   describe "#show" do
-    it { requires_signed_in_user_to { get :show } }
-
     context "with no query" do
       it "should not fire an analytics event" do
         get :show
