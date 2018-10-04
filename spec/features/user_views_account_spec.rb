cbd0eafcd74a3a3242f142700197085fa94bf3a7
Chris Toomey <chris@ctoomey.com>
2018-10-04 13:00:33 -0400

    Remove billing & upgrade content from profile page

--- spec/features/user_views_account_spec.rb
+++ spec/features/user_views_account_spec.rb
@@ -24,21 +24,6 @@ feature "Account Settings" do
     expect(field_value_from_name("Name")).to eq "Change Name"
   end
 
-  scenario "user edits address information" do
-    user = create(:user, name: "Test User")
-
-    visit edit_my_account_path(as: user)
-
-    fill_in "Address 1", with: "New Address"
-    click_button "Update address"
-
-    expect(page).to have_content I18n.t('users.flashes.update.success')
-
-    visit edit_my_account_path(as: user)
-
-    expect(field_value_from_name("Address 1")).to eq "New Address"
-  end
-
   private
 
   def field_value_from_name(name)
