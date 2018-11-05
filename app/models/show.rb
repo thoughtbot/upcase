41832d58619b7bceb4b4aeb2cb8705d955b3c26d
Matt Jankowski <mjankowski@thoughtbot.com>
2018-11-05 10:47:41 -0500

    Remove unused `Show#latest_video` method

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,8 +4,4 @@ class Show < Product
   def self.the_weekly_iteration
     where(name: THE_WEEKLY_ITERATION).first
   end
-
-  def latest_video
-    videos.published.recently_published_first.first
-  end
 end
--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -12,26 +12,4 @@ describe Show do
       expect(result).to eq show
     end
   end
-
-  describe "#latest_video" do
-    it "returns its latest, published video" do
-      show = create(:show)
-      other_show = create(:show)
-      create_video "older_published", show: show, published_on: 2.days.ago
-      create_video "latest_published", show: show, published_on: 1.day.ago
-      create_video "unpublished", show: show, published_on: 1.day.from_now
-      create_video "other_show", show: other_show, published_on: Time.current
-
-      expect(show.latest_video.name).to eq("latest_published")
-    end
-
-    def create_video(name, show:, published_on:)
-      create(
-        :video,
-        name: name,
-        watchable: show,
-        published_on: published_on
-      )
-    end
-  end
 end
