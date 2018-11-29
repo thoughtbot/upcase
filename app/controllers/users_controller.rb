d04a60c7f6addabc91e2e92c13e85b6f253dd14b
Matt Jankowski <mjankowski@thoughtbot.com>
2018-11-29 11:03:25 -0500

    Clean up users controller param filtering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,25 +5,27 @@ class UsersController < Clearance::UsersController
   end
 
   def update
-    if current_user.update_attributes(create_user_from_params)
+    if current_user.update_attributes(filtered_user_params)
       redirect_to my_account_path, notice: I18n.t("users.flashes.update.success")
     else
       render :edit
     end
   end
 
-  def create_user_from_params
-    user_params = params.require(:user).permit(
+  def filtered_user_params
+    if user_params[:password].present?
+      user_params
+    else
+      user_params.except(:password)
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(
       :email, :password, :name, :github_username, :bio, :organization,
       :address1, :address2, :city, :state, :zip_code, :country,
       :unsubscribed_from_emails
     )
-
-    unless user_params[:password].present?
-      user_params.delete(:password)
-    end
-
-    user_params
   end
 
   def return_to
