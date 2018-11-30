0ba12c452cf13a9825faed4b3923c54f61a7eece
Matt Jankowski <mjankowski@thoughtbot.com>
2018-11-30 11:13:22 -0500

    Do not page cache the paginated shows show view

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,6 +1,4 @@
 class ShowsController < ApplicationController
-  cache_signed_out_action :show
-
   def show
     @show = find_show
     @video_listing = VideoListing.new(sorted_published_videos, current_user)
