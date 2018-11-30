fdf36b917b10f7da52fa954855f22eda51c0d1b8
Matt Jankowski <mjankowski@thoughtbot.com>
2018-11-30 11:17:48 -0500

    Allow caching flag in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,9 +6,21 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Show full error reports and disable caching
+  # Show full error reports
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+    config.cache_store = :null_store
+  end
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
