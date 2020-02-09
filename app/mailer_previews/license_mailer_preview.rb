1db7b510ed39b84674165322f8b4192a82b482f6
fatkodima <fatkodima123@gmail.com>
2020-02-09 17:26:06 +0200

    Remove unused LicenseMailer mailer

--- app/mailer_previews/license_mailer_preview.rb
+++
@@ -1,8 +0,0 @@
-class LicenseMailerPreview < ActionMailer::Preview
-  def fullfillment_error
-    user = User.new(name: "John Doe")
-    repository = Repository.first
-
-    LicenseMailer.fulfillment_error(repository, user)
-  end
-end
