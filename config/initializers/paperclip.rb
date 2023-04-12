c7f9e45bc259fa6f3a53759ed0c2779391787a2c
Summer Megel <summer@thoughtbot.com>
2023-04-12 16:45:26 -0600

    Monkey-patch removed `URI.escape` method within Paperclip
    `URI.escape` was obsoleted in Ruby 1.9.2, and it was removed in Ruby
    3.0.

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,1 @@
+require Rails.root.join("lib/paperclip/url_generator.rb")
---
+++ lib/paperclip/url_generator.rb
@@ -0,0 +1,15 @@
+module Paperclip
+  class UrlGenerator
+    # `URI.escape` was obsoleted in Ruby 1.9 and removed in Ruby 2.7; this
+    # replaces (within the context of only this class) `URI` with
+    # `::URI::DEFAULT_PARSER` (which is defined as `::URI::RFC2396_Parser.new),
+    # which is what `URI.escape` used before it was removed.
+    #
+    # This would be a dangerous solution in most other cases, since it would
+    # affect all references to `URI` within a class/module, but it's safe here
+    # because Paperclip is deprecated and therefore we know that it will never
+    # be updated.
+    URI = ::URI::DEFAULT_PARSER
+    private_constant :URI
+  end
+end
