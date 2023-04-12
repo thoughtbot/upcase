d0e52ffbdd232ebf42da8ef14962301801a08793
Summer Megel <summer@thoughtbot.com>
2023-04-12 14:19:18 -0600

    Break out `add_attachment` into its components due to Ruby 3 incompatibility

--- db/migrate/20130719134342_add_attachment_to_episodes.rb
+++ db/migrate/20130719134342_add_attachment_to_episodes.rb
@@ -1,6 +1,9 @@
 class AddAttachmentToEpisodes < ActiveRecord::Migration[4.2]
   def change
-    add_attachment :episodes, :mp3
+    add_column :episodes, :mp3_file_name, :string
+    add_column :episodes, :mp3_content_type, :string
+    add_column :episodes, :mp3_file_size, :integer
+    add_column :episodes, :mp3_updated_at, :datetime
     remove_column :episodes, :file
   end
 end
