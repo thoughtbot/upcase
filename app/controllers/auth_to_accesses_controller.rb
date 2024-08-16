d5be99d9d5aaf2b4267bb3b8afe37aec2de3858b
Justin Toniazzo <jt@thoughtbot.com>
2024-08-16 10:43:20 -0400

    undo unnecessary

--- app/controllers/auth_to_accesses_controller.rb
+++ app/controllers/auth_to_accesses_controller.rb
@@ -1,6 +1,5 @@
 class AuthToAccessesController < ApplicationController
   def show
-    @video = find_video
     save_video_slug
     redirect_to github_auth_with_video_origin_path
   end
@@ -8,14 +7,14 @@ class AuthToAccessesController < ApplicationController
   private
 
   def save_video_slug
-    session[:auth_to_access_video_slug] = @video.slug
+    session[:auth_to_access_video_slug] = video.slug
   end
 
   def github_auth_with_video_origin_path
-    github_auth_path(origin: video_path(@video))
+    github_auth_path(origin: video_path(video))
   end
 
-  def find_video
-    Video.find(params[:video_id])
+  def video
+    @video = Video.find(params[:video_id])
   end
 end
