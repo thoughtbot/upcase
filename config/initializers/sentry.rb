8ee8bcd68bef60f679ae2c2a457b255f879e02aa
Chad Pytel <chad@thoughtbot.com>
2024-11-26 10:50:32 -0500

    Revert "Start sampling for performance"
    This reverts commit 16e9c53396c20b49f55b8724d77adb2825ea7b5d.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,5 +5,5 @@ Sentry.init do |config|
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
-  config.traces_sample_rate = 0.1
+  config.traces_sample_rate = 0
 end
