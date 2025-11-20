9d44487774aadac00d6e8c4a14d53909cb1bf905
Summer ☀️ <summer@thoughtbot.com>
2025-11-20 00:40:19 -0700

    Add missing specs for marketing redesign opportunity creation form

---
+++ spec/controllers/marketing_redesign/opportunities_controller_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+describe MarketingRedesign::OpportunitiesController do
+  around do |example|
+    ClimateControl.modify(ENABLE_MARKETING_REDESIGN: "true") do
+      Rails.application.reload_routes!
+      example.run
+    end
+  ensure
+    Rails.application.reload_routes!
+  end
+
+  describe "#create" do
+    it "calls the Hub API opportunities creation endpoint with the correct parameters" do
+      allow(Hub::Opportunities).to receive(:create)
+
+      post(
+        :create,
+        params: {
+          opportunity: {
+            contact_first_name: "Firstname",
+            contact_last_name: "Lastname",
+            email: "example@example.com",
+            company_name: "Example Company",
+            contact_job_title: "Example Job Title"
+          }
+        }
+      )
+
+      expect(Hub::Opportunities).to(
+        have_received(:create).with({
+          "contact_name" => "Firstname Lastname",
+          "email" => "example@example.com",
+          "company_name" => "Example Company",
+          "contact_job_title" => "Example Job Title",
+          :conversion_point => be_present,
+          :project_type => be_present
+        })
+      )
+    end
+  end
+end
