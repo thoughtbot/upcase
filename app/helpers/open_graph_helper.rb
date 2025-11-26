a06aebcc38dcbcf21f2e1be2cf35dc7b5388dd37
Alex Chen <achen178@gmail.com>
2025-11-26 17:12:35 -0500

    Fix bugs and general css improvements (#2833)
    * Increase specificity on testimonial logo styles
    
    * Fix alt text for avatar photo
    
    * Update opengraph preview logo
    
    * Update footer responsiveness
    
    * Responsive header and navigation menu

--- app/helpers/open_graph_helper.rb
+++ app/helpers/open_graph_helper.rb
@@ -1,7 +1,7 @@
 module OpenGraphHelper
   def open_graph_tags
     raw [
-      tag("meta", property: "og:image", content: image_url("ralph-gradient.png")),
+      tag("meta", property: "og:image", content: image_url("thoughtbot_horizontal_default_red_on_light.png")),
       tag("meta", property: "og:url", content: url_for(only_path: false)),
       tag("meta", property: "og:title", content: page_title)
     ].join("\n")
